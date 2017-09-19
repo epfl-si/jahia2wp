2a493643746889952847a8b6d1941b6fda938242
GregLeBarbar <gregory.charmier.epfl.ch>
2017-09-19 18:40:56 +0200

    Example of mysql cmd

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -22,6 +22,12 @@ class Generator:
         sites = Utils.csv_to_dict(file_path=csv_file)
 
         for site in sites:
+
+            # Exemple de commande mysql
+            cmd = "mysql -h localhost -u root --password=root -e \"CREATE USER toto1 IDENTIFIED WITH mysql_native_password AS '*0D1CED9BEC10A777AEC23CCC353A8C08A633045E';\""
+            subprocess.check_output(cmd, shell=True)
+
+            # Exemple de commande wp-cli
             command = "core download --version=4.8"
             env_wp_path = Utils.get_mandatory_env(key="WP_PATH")
             path = env_wp_path + "/htdocs"
