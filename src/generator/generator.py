0b84e5033fe5e4d87342fbde311bf4af96893bd1
GregLeBarbar <gregory.charmier.epfl.ch>
2017-09-20 14:59:34 +0200

    Generate WP sites - Work in progress

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -7,6 +7,24 @@ from utils import Utils
 class Generator:
 
     @staticmethod
+    def set_environment_variables():
+
+        return {
+            'mysql_wp_user':  Utils.get_mandatory_env(key="MYSQL_WP_USER"),
+            'mysql_wp_password': Utils.get_mandatory_env(key="MYSQL_WP_PASSWORD"),
+            'mysql_db_host': Utils.get_mandatory_env(key="MYSQL_DB_HOST"),
+            'mysql_super_user': Utils.get_mandatory_env(key="MYSQL_SUPER_USER"),
+            'mysql_super_password': Utils.get_mandatory_env(key="MYSQL_SUPER_PASSWORD"),
+            'wp_db_name': Utils.get_mandatory_env(key="WP_DB_NAME"),
+            'wp_env': Utils.get_mandatory_env(key="WP_ENV"),
+            'site_path': Utils.get_mandatory_env(key="SITE_PATH"),
+            'wp_title': Utils.get_mandatory_env(key="WP_TITLE"),
+            'wp_admin_user': Utils.get_mandatory_env(key="WP_ADMIN_USER"),
+            'wp_admin_password': Utils.get_mandatory_env(key="WP_ADMIN_PASSWORD"),
+            'wp_admin_email': Utils.get_mandatory_env(key="WP_ADMIN_EMAIL"),
+        }
+
+    @staticmethod
     def wp_cli(command, path, site):
         try:
             cmd = "wp {} --path='{}'".format(command, path)
@@ -16,19 +34,56 @@ class Generator:
             logging.error("%s - WP export - wp_cli failed : %s", site, err)
             return None
 
+    @staticmethod
+    def run_command(command, site):
+        try:
+            subprocess.check_output(command, shell=True)
+            logging.debug("Generator - {0} - Run command {1}".format(site, command))
+        except subprocess.CalledProcessError as err:
+            logging.error("Generator - {0} - Command {1} failed".format(site, command))
+            return False
+
     @classmethod
     def run(cls, csv_file):
 
-        sites = Utils.csv_to_dict(file_path=csv_file)
+        sites = Utils.csv_to_dict(file_path=csv_file, delimiter=';')
+
+        env_vars = cls.set_environment_variables()
 
         for site in sites:
 
-            # Exemple de commande mysql
-            cmd = "mysql -h localhost -u root --password=root -e \"CREATE USER toto1 IDENTIFIED WITH mysql_native_password AS '*0D1CED9BEC10A777AEC23CCC353A8C08A633045E';\""
-            subprocess.check_output(cmd, shell=True)
+            mysql_connection_string = "@mysql -h {mysql_db_host} -u {mysql_super_user} " \
+                                    "--password={mysql_super_password}".format(**env_vars)
 
-            # Exemple de commande wp-cli
-            command = "core download --version=4.8"
-            env_wp_path = Utils.get_mandatory_env(key="WP_PATH")
-            path = env_wp_path + "/htdocs"
-            cls.wp_cli(command, path, site=site["wp_site_title"])
+            # create MySQL user
+            command = mysql_connection_string
+            command += "-e \"CREATE USER '{mysql_wp_user}' IDENTIFIED BY '{mysql_wp_password}';\"".format(**env_vars)
+            cls.run_command(command, site=site["wp_site_title"])
+
+            # grant privileges
+            command = mysql_connection_string
+            command += "-e \"GRANT ALL PRIVILEGES ON \`{wp_db_name}\`.* TO \`{mysql_wp_user}\`@'%';".format(**env_vars)
+            cls.run_command(command, site=site["wp_site_title"])
+
+            # create htdocs path
+            command = "mkdir -p /srv/{wp_env}/{site_path}/htdocs".format(**env_vars)
+            cls.run_command(command, site=site["wp_site_title"])
+
+            # install WordPress 4.8
+            command = "wp core download --version=4.8 --path=/srv/{wp_env}/{site_path}/htdocs".format(**env_vars)
+            cls.run_command(command, site=site["wp_site_title"])
+
+            # config WordPress
+            command = "wp config create --dbname={wp_db_name} --dbuser={mysql_wp_user}".format(**env_vars)
+            command += " --dbpass={mysql_wp_password} --dbhost={mysql_db_host}".format(**env_vars)
+            command += " --path=/srv/{wp_env}/{site_path}/htdocs".format(**env_vars)
+            cls.run_command(command, site=site["wp_site_title"])
+
+            #
+            command = "wp db create --path=/srv/{wp_env}/{site_path}/htdocs".format(**env_vars)
+            cls.run_command(command, site=site["wp_site_title"])
+
+            command = "wp --allow-root core install --url=http://{site_path} --title={wp_title}".format(**env_vars)
+            command += " --admin_user={wp_admin_user} --admin_password={wp_admin_password}".format(**env_vars)
+            command += " --admin_email={wp_admin_email} --path=/srv/{wp_env}/{site_path}/htdocs".format(**env_vars)
+            cls.run_command(command, site=site["wp_site_title"])
