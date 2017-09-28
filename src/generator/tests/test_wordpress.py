8699e6b43fa3b0688abd0c4cb10a13a87f36495a
ebreton <manu@ibimus.com>
2017-09-28 16:10:53 +0200

    rappatriement des tests de Manu J.

---
+++ src/generator/tests/test_wordpress.py
@@ -0,0 +1,115 @@
+import datetime
+import pytest
+import uuid
+import logging
+import os
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class Test_WPTester(object):
+    @pytest.fixture()
+    def base_url(self):
+        return os.getenv('SUT_BASE_URL', 'http://localhost:8000')
+
+    @pytest.fixture()
+    def username(self):
+        return os.getenv('SUT_USERNAME', 'admin')
+
+    @pytest.fixture()
+    def password(self):
+        return os.getenv("SUT_PASSWORD", 'admin')
+
+    @pytest.fixture()
+    def session(self):
+        logging.debug("Starting new session")
+        session = requests.session()
+        yield session
+        logging.debug("Closing session")
+        session.close()
+
+    def test_upload_image_to_wordpress(self, session, base_url, username, password):
+        # login to WordPress
+        logging.debug("Login in to WordPress")
+        link = "{base_path}/wp-login.php".format(base_path=base_url)
+        login_data = {"log": username,
+                      "pwd": password,
+                      "rememberme": "forever",
+                      "redirect_to": "{base_path}/wp-admin".format(base_path=base_url),
+                      "redirect_to_automatic": "1"
+                      }
+        page_login = session.post(link, login_data)
+        assert page_login.status_code is 200
+
+        # check that the user is correctly logged in (i.e. his name shows up correctly on the page
+        logging.debug("Checking that the user is correctly logged in")
+        soup = BeautifulSoup(page_login.content, "lxml")
+        logged_in_user_element = soup.find('span', {'class': 'display-name'})
+        logged_in_user_display_name = logged_in_user_element.text if logged_in_user_element else ''
+        assert logged_in_user_display_name == username
+
+        # go the media upload page (in order to generate a nonce)
+        logging.debug("Getting the media upload page in order to get wp_nonce")
+        upload_page = session.get('{base_path}/wp-admin/media-new.php'.format(base_path=base_url))
+
+        # Grab the _wp_nonce
+        logging.debug("Getting the wp_nonce")
+        soup = BeautifulSoup(upload_page.content, 'lxml')
+        wp_nonce_element = soup.find('input', {'name': '_wpnonce'})
+        wp_nonce = wp_nonce_element.get('value')
+
+        # generates a unique file name in order to make sure that there is no collision while uploading
+        logging.debug("Generating a unique file name")
+        unique_file_name = str(uuid.uuid1()) + '.jpg'
+
+        # upload the image
+        logging.debug("Uploading the image")
+        with open('2.jpg', 'rb') as f:
+            upload_data = {'post_id': '0',
+                           '_wp_http_referer': '/wp-admin/media-new.php',
+                           '_wpnonce': wp_nonce,
+                           'action': 'upload_attachement',
+                           'html-upload': 'Upload'}
+            files = {'async-upload': (unique_file_name, f)}
+            upload_result = session.post('{base_path}/wp-admin/media-new.php'.format(base_path=base_url),
+                                         data=upload_data,
+                                         files=files)
+        assert upload_result.status_code is 200
+
+        # checks if the image has really been uploaded
+        logging.debug("Cross cjecking that the image was really uploaded")
+        image_url = "{base_path}/wp-content/uploads/{timestamp:%Y/%m}/{filename}".format(
+            base_path=base_url,
+            timestamp=datetime.date.today(),
+            filename=unique_file_name)
+        image_page = session.get(image_url)
+        assert image_page.status_code is 200
+
+        # cleanup up the mess
+        logging.debug("Starting cleanup the uploaded file")
+        upload_management_page_url = "{base_path}/wp-admin/upload.php?mode=list".format(
+            base_path=base_url)
+        upload_management_page = session.get(upload_management_page_url)
+        assert upload_management_page.status_code is 200
+        soup = BeautifulSoup(upload_management_page.content, 'lxml')
+
+        # get list of filenames
+        logging.debug("Getting the list of filenames")
+        filename_elements = soup.find_all('p', {'class': 'filename'})
+        assert len(filename_elements) > 0
+        for filename_element in filename_elements:
+            if unique_file_name in filename_element.text:
+                table_cell_element = filename_element.parent
+                delete_link = table_cell_element.find('a', {'class': 'submitdelete'})
+                delete_link = delete_link['href']
+                delete_link = "{base_path}/wp-admin/{link}".format(base_path=base_url, link=delete_link)
+
+                logging.debug("Deleting the file")
+                delete_media_result = session.get(delete_link)
+                assert delete_media_result.status_code is 200
+
+        # checks if the image has really been cleaned
+        logging.debug("Cross checking that the file has been correctly deleted")
+        image_page = session.get(image_url)
+        assert image_page.status_code == 404
