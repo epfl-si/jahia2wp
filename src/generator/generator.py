837b4f5d66ff58a1c77866ffb85bfca1e4a20bd2
ebreton <manu@ibimus.com>
2017-10-01 10:10:19 +0200

    refactored generator class

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -9,53 +9,26 @@ from epflldap.ldap_search import get_username, get_email
 from utils import Utils
 
 
-class WPGenerator:
+class WPSite:
 
-    USER_NAME_LENGTH = 32
-    DB_NAME_LENGTH = 32
-    PASSWORD_LENGTH = 32
     PROTOCOL = "http"
-
-    MYSQL_DB_HOST = Utils.get_mandatory_env(key="MYSQL_DB_HOST")
-    MYSQL_SUPER_USER = Utils.get_mandatory_env(key="MYSQL_SUPER_USER")
-    MYSQL_SUPER_PASSWORD = Utils.get_mandatory_env(key="MYSQL_SUPER_PASSWORD")
-
+    DEFAULT_TITLE = "New WordPress"
     WP_VERSION = Utils.get_mandatory_env(key="WP_VERSION")
-    WP_ADMIN_USER = Utils.get_mandatory_env(key="WP_ADMIN_USER")
-    WP_ADMIN_EMAIL = Utils.get_mandatory_env(key="WP_ADMIN_EMAIL")
 
-    def __init__(self,
-                 openshift_env=None,
-                 wp_site_url=None,
-                 wp_default_site_title=None,
-                 owner_id=None,
-                 responsible_id=None):
-
+    def __init__(self, openshift_env, wp_site_url, wp_default_site_title=None):
+        # extract domain and folder from given url
         url = urlparse(wp_site_url)
 
+        # TODO: use validators from veritas to validate openshift_env
         self.openshift_env = openshift_env
+
+        # set WP informations
         self.domain = url.netloc.strip('/')
         self.folder = url.path.strip('/')
-        self.wp_default_site_title = wp_default_site_title
-        self.set_users_info(owner_id, responsible_id)
-        self.set_unique_vars()
+        self.wp_default_site_title = wp_default_site_title or self.DEFAULT_TITLE
 
     def __repr__(self):
-        return "{}/{}/{}".format(self.openshift_env, self.domain, self.folder)
-
-    def set_users_info(self, owner_id, responsible_id):
-        self.owner_username = get_username(sciper=owner_id)
-        self.owner_email = get_email(sciper=owner_id)
-        self.responsible_username = get_username(sciper=responsible_id)
-        self.responsible_email = get_email(sciper=responsible_id)
-
-    def set_unique_vars(self):
-        self.mysql_wp_user = Utils.generate_random_b64(self.USER_NAME_LENGTH).lower()
-        self.mysql_wp_password = Utils.generate_password(self.PASSWORD_LENGTH)
-        self.wp_db_name = Utils.generate_random_b64(self.DB_NAME_LENGTH).lower()
-        self.wp_admin_password = Utils.generate_password(self.PASSWORD_LENGTH)
-        self.wp_webmaster_password = Utils.generate_password(self.PASSWORD_LENGTH)
-        self.wp_responsible_password = Utils.generate_password(self.PASSWORD_LENGTH)
+        return "WP@{}/{}/{}".format(self.openshift_env, self.domain, self.folder)
 
     @property
     def path(self):
@@ -64,6 +37,125 @@ class WPGenerator:
     @property
     def url(self):
         return "{0.PROTOCOL}://{0.domain}/{0.folder}".format(self)
+
+
+class WPUser:
+
+    WP_PASSWORD_LENGTH = 32
+
+    def __init__(self, username, email, password=None):
+        # TODO: use validators from veritas to validate both username and email
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        password_string = 'xxxx' if self.password is not None else 'None'
+        return "{0.username}:{0.email} <{1}>".format(self, password_string)
+
+    @classmethod
+    def from_sciper(cls, sciper_id):
+        return cls(
+            username=get_username(sciper=sciper_id),
+            email=get_email(sciper=sciper_id)
+        )
+
+    def set_password(self, password=None):
+        self.password = password or Utils.generate_password(self.WP_PASSWORD_LENGTH)
+
+
+class WPRawConfig:
+
+    def __init__(self, wordpress):
+        self.wordpress = wordpress
+
+    def __repr__(self):
+        installed_string = '[ok]' if self.is_installed else '[ko]'
+        return "config {0} for {1}".format(installed_string, repr(self.wordpress))
+
+    def run_wp_cli(self, command):
+        try:
+            cmd = "wp {} --path='{}'".format(command, self.wordpress.path)
+            logging.debug("exec '%s'", cmd)
+            return subprocess.check_output(cmd, shell=True)
+        except subprocess.CalledProcessError as err:
+            logging.error("%s - WP export - wp_cli failed : %s", repr(self.wordpress), err)
+            return None
+
+    @property
+    def is_installed(self):
+        return os.path.isdir(self.wordpress.path)
+
+    @property
+    def is_config_valid(self):
+        if not self.is_installed:
+            return False
+        # TODO: check that the config is working (DB and user ok)
+        # wp-cli command (status?)
+
+    @property
+    def is_install_valid(self):
+        if not self.is_config_valid():
+            return False
+        # TODO: check that the site is available, that user can login and upload media
+        # tests from test_wordpress
+
+    def add_wp_user(self, username, email):
+        return self._add_user(WPUser(username, email))
+
+    def add_ldap_user(self, sciper_id):
+        return self._add_user(WPUser.from_sciper(sciper_id))
+
+    def _add_user(self, user):
+        if not user.password:
+            user.set_password()
+        # TODO: call wp-cli to add user in WP
+        return user
+
+
+class WPGenerator:
+
+    DB_NAME_LENGTH = 32
+    MYSQL_USER_NAME_LENGTH = 32
+    MYSQL_PASSWORD_LENGTH = 32
+
+    MYSQL_DB_HOST = Utils.get_mandatory_env(key="MYSQL_DB_HOST")
+    MYSQL_SUPER_USER = Utils.get_mandatory_env(key="MYSQL_SUPER_USER")
+    MYSQL_SUPER_PASSWORD = Utils.get_mandatory_env(key="MYSQL_SUPER_PASSWORD")
+
+    WP_ADMIN_USER = Utils.get_mandatory_env(key="WP_ADMIN_USER")
+    WP_ADMIN_EMAIL = Utils.get_mandatory_env(key="WP_ADMIN_EMAIL")
+
+    def __init__(self, openshift_env, wp_site_url, wp_default_site_title, owner_id, responsible_id):
+        # create WordPress site and config
+        self.wp_site = WPSite(openshift_env, wp_site_url, wp_default_site_title)
+        self.wp_config = WPRawConfig(self.wp_site)
+
+        # prepare admin for exploitation/maintenance
+        self.wp_admin = WPUser(self.WP_ADMIN_USER, self.WP_ADMIN_EMAIL)
+        self.wp_admin.set_password()
+
+        # store scipers_id for later
+        self.owner_id = owner_id
+        self.responsible_id = responsible_id
+
+        # create mysql credentials
+        self.wp_db_name = Utils.generate_random_b64(self.DB_NAME_LENGTH).lower()
+        self.mysql_wp_user = Utils.generate_random_b64(self.MYSQL_USER_NAME_LENGTH).lower()
+        self.mysql_wp_password = Utils.generate_password(self.MYSQL_PASSWORD_LENGTH)
+
+    def __repr__(self):
+        return "generator for {}".format(repr(self.wordpress))
+
+    def generate(self):
+        # create specific mysql db and user
+        self.prepare_db()
+
+        # download, config and install WP
+        self.install_wp()
+
+        # add 2 given webmasters
+        self.add_webmasters()
 
     def run_command(self, command):
         try:
@@ -78,37 +170,7 @@ class WPGenerator:
             " --password={0.MYSQL_SUPER_PASSWORD} ".format(self)
         self.run_command(mysql_connection_string + command)
 
-    def run_wp_cli(self, command):
-        try:
-            cmd = "wp {} --path='{}'".format(command, self.path)
-            logging.debug("exec '%s'", cmd)
-            return subprocess.check_output(cmd, shell=True)
-        except subprocess.CalledProcessError as err:
-            logging.error("%s - WP export - wp_cli failed : %s",
-                          repr(self), err)
-            return None
-
-    def is_installed(self):
-        return os.path.isdir(self.path)
-
-    def is_config_valid(self):
-        if not self.is_installed():
-            return False
-        # TODO EB: check that the config is working (DB and user ok)
-        # wp-cli command (status?)
-
-    def is_install_valid(self):
-        if not self.is_config_valid():
-            return False
-        # TODO EB : check that the site is available, that user can login and upload media
-        # tests from test_wordpress
-
-    def generate(self):
-        # check we have a clean place first
-        if self.is_installed():
-            logging.error("%s - WP export - wordpress files already found", repr(self))
-            return False
-
+    def prepare_db(self):
         # create htdocs path
         self.run_command("mkdir -p /srv/{0.openshift_env}/{0.domain}/htdocs/{0.folder}".format(self))
 
@@ -120,8 +182,14 @@ class WPGenerator:
         command = "-e \"GRANT ALL PRIVILEGES ON \`{0.wp_db_name}\`.* TO \`{0.mysql_wp_user}\`@'%';\""
         self.run_mysql(command.format(self))
 
+    def install_wp(self):
+        # check we have a clean place first
+        if self.wp_config.is_installed:
+            logging.error("%s - WP export - wordpress files already found", repr(self))
+            return False
+
         # install WordPress 4.8
-        self.run_wp_cli("core download --version=4.8".format(self))
+        self.wp_config.run_wp_cli("core download --version=4.8")
 
         # config WordPress
         command = "config create --dbname='{0.wp_db_name}' --dbuser='{0.mysql_wp_user}'" \
@@ -133,15 +201,19 @@ class WPGenerator:
 
         # fill out first form in install process (setting admin user and permissions)
         command = "--allow-root core install --url={0.url} --title='{0.wp_default_site_title}'" \
-            " --admin_user={0.WP_ADMIN_USER} --admin_password='{0.wp_admin_password}'"\
-            " --admin_email='{0.WP_ADMIN_EMAIL}'"
-        self.run_wp_cli(command.format(self))
+            " --admin_user={1.username} --admin_password='{1.password}'"\
+            " --admin_email='{1.email}'"
+        self.run_wp_cli(command.format(self, self.wp_admin))
+
+    def add_webmasters(self):
+        owner = self.wp_config.add_ldap_user(self.owner_id)
+        responsible = self.wp_config.add_ldap_user(self.responsible_id)
+        return (owner, responsible)
 
 
 class MockedWPGenerator(WPGenerator):
 
-    def set_users_info(self, owner_id, responsible_id):
-        self.owner_username = "owner"
-        self.owner_email = "owner@epfl.ch"
-        self.responsible_username = "reponsible"
-        self.responsible_email = "responsible@epfl.ch"
+    def add_webmasters(self):
+        owner = self.wp_config.add_wp_user("owner", "owner@epfl.ch")
+        responsible = self.wp_config.add_wp_user("responsible", "responsible@epfl.ch")
+        return (owner, responsible)
--- src/generator/tests/test_generator.py
+++ src/generator/tests/test_generator.py
@@ -1,24 +1,74 @@
-import logging
+import pytest
 
 from utils import Utils
-from generator.generator import MockedWPGenerator
+from generator.generator import WPSite, WPUser, WPRawConfig, MockedWPGenerator
 
 
-def test_generate():
-    generator = MockedWPGenerator(
-        openshift_env=Utils.get_mandatory_env(key="WP_ENV"),
-        wp_site_url="https://localhost/folder",
-        wp_default_site_title="My test",
-        owner_id="157489",
-        responsible_id="157489")
+class TestWPSite:
 
-    generator.generate()
+    @pytest.fixture(scope='module')
+    def wordpress(self):
+        return WPSite(
+            openshift_env="test",
+            wp_site_url="https://localhost/folder",
+            wp_default_site_title="My test")
+
+    def test_path(self, wordpress):
+        assert wordpress.path == "/srv/test/localhost/htdocs/folder"
+
+    def test_url(self, wordpress):
+        assert wordpress.url == "http://localhost/folder"
 
 
-def main():
-    logging.basicConfig(level=logging.DEBUG)
-    test_generate()
+class TestWPUser:
+
+    def test_password(self):
+        user = WPUser("test", "test@example.com")
+        assert user.password is None
+        user.set_password()
+        assert len(user.password) == WPUser.WP_PASSWORD_LENGTH
+
+    def test_from_sciper(self):
+        # TODO: mock LDAP calls get_username & get_email
+        pass
 
 
-if __name__ == '__main__':
-    main()
+class TestWPRawConfig:
+
+    @pytest.fixture()
+    def wp_config(self):
+        wordpress = WPSite(
+            openshift_env="test",
+            wp_site_url="https://localhost/folder",
+            wp_default_site_title="My test")
+        return WPRawConfig(wordpress)
+
+    def test_is_installed(self, wp_config):
+        pass
+
+    def test_is_install_valid(self, wp_config):
+        pass
+
+    def test_add_wp_user(self, wp_config):
+        pass
+
+
+class TestWPGenerator:
+
+    @pytest.fixture()
+    def wp_generator(self):
+        return MockedWPGenerator(
+            openshift_env=Utils.get_mandatory_env(key="WP_ENV"),
+            wp_site_url="https://localhost/folder",
+            wp_default_site_title="My test",
+            owner_id="157489",
+            responsible_id="157489")
+
+    def test_prepare_db(self, wp_generator):
+        pass
+
+    def test_install_wp(self, wp_generator):
+        pass
+
+    def test_generate(self, wp_generator):
+        pass
