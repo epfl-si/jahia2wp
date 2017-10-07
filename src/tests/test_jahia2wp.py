3ac09772c8db8b389c3ebeb9eb64ee8553b3fb44
ebreton <manu@ibimus.com>
2017-10-07 22:22:48 +0200

    added CLI tests for wp-version & wp-admins

--- src/tests/test_jahia2wp.py
+++ src/tests/test_jahia2wp.py
@@ -3,7 +3,7 @@ import os
 import pytest
 
 from utils import Utils
-from wordpress import WPGenerator
+from wordpress import WPGenerator, WPUser
 
 
 CURRENT_DIR = os.path.dirname(__file__)
@@ -66,6 +66,18 @@ class TestCommandLine:
     def test_check_one_success(self):
         expected = "WordPress site valid and accessible at http://localhost/unittest"
         assert Utils.run_command('python %s check-one $WP_ENV http://localhost/unittest' % SCRIPT_FILE) == expected
+
+    def test_wp_version(self):
+        expected = Utils.get_mandatory_env(key="WP_VERSION")
+        assert Utils.run_command('python %s wp-version $WP_ENV http://localhost/unittest' % SCRIPT_FILE) == expected
+
+    def test_wp_admins(self):
+        user = WPUser(
+            Utils.get_mandatory_env(key="WP_ADMIN_USER"),
+            Utils.get_mandatory_env(key="WP_ADMIN_EMAIL"),
+            role='administrator')
+        expected = repr(user)
+        assert Utils.run_command('python %s wp-admins $WP_ENV http://localhost/unittest' % SCRIPT_FILE) == expected
 
     def test_clean_one(self):
         assert Utils.run_command('python %s clean-one $WP_ENV http://localhost/unittest' % SCRIPT_FILE)
