8d2fc5ee6cc58687bddf64ba14d4fb336ceeb5ad
ebreton <manu@ibimus.com>
2017-10-09 21:39:23 +0200

    added tests for get_*_env

--- src/tests/test_jahia2wp.py
+++ src/tests/test_jahia2wp.py
@@ -7,36 +7,10 @@ from wordpress import WPGenerator, WPUser
 
 
 CURRENT_DIR = os.path.dirname(__file__)
-TEST_FILE = 'csv_fixture.csv'
-
 SRC_DIR = os.path.abspath(os.path.join(CURRENT_DIR, '..'))
 SCRIPT_FILE = os.path.join(SRC_DIR, 'jahia2wp.py')
 
 TEST_ENV = 'test'
-
-EXPECTED_OUTPUT_FROM_CSV = [
-        {'key': 'table_prefix', 'value': 'wp_', 'type': 'variable'},
-        {'key': 'DB_NAME', 'value': 'wp_a0veseethknlxrhdaachaj5qgdixh', 'type': 'constant'},
-        {'key': 'DB_USER', 'value': 'ogtc,62msegz2beji', 'type': 'constant'},
-        {'key': 'DB_PASSWORD', 'value': 'Rfcua2LKD^vpGy@m*R*Z', 'type': 'constant'},
-        {'key': 'DB_COLLATE', 'value': '', 'type': 'constant'}
-    ]
-
-
-class TestUtils:
-
-    def test_csv_from_filepath(self):
-        file_path = os.path.join(CURRENT_DIR, TEST_FILE)
-        assert Utils.csv_filepath_to_dict(file_path) == EXPECTED_OUTPUT_FROM_CSV
-
-    def test_csv_from_string(self):
-        text = """key,value,type
-table_prefix,wp_,variable
-DB_NAME,wp_a0veseethknlxrhdaachaj5qgdixh,constant
-DB_USER,"ogtc,62msegz2beji",constant
-DB_PASSWORD,Rfcua2LKD^vpGy@m*R*Z,constant
-DB_COLLATE,,constant"""
-        assert Utils.csv_string_to_dict(text) == EXPECTED_OUTPUT_FROM_CSV
 
 
 @pytest.fixture(scope="module")
