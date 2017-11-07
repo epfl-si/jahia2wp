6fc4f0e08c2f1c4a1a5ec3dee50081deb508c598
LuluTchab <l.chaboudez@gmail.com>
2017-11-07 15:15:56 +0100

    Specific site name incorrect, tests for WPPluginList added

--- src/wordpress/tests/test_plugins.py
+++ src/wordpress/tests/test_plugins.py
@@ -1,19 +1,56 @@
+import pytest
+from settings import *
+from wordpress.plugins import *
+
 def test_yaml_include():
     pass
 
 
+
+@pytest.fixture(scope="module")
+def wp_plugin_list():
+    return WPPluginList(PLUGINS_CONFIG_GENERIC_FOLDER, 'config-lot1.yml', PLUGINS_CONFIG_SPECIFIC_FOLDER)
+
+@pytest.fixture(scope="module")
+def wp_site():
+    return WPSite(
+        openshift_env="test",
+        wp_site_url="https://localhost/unittest",
+        wp_default_site_title="My test")
+
+
 class TestWPPluginList:
 
-    def test_generic_list(self):
-        pass
+    def test_generic_list(self, wp_plugin_list):
+        assert len(wp_plugin_list.plugins()) == 1
 
-    def test_list_for_site(self):
+    def test_list_for_site(self, wp_plugin_list):
+        assert len(wp_plugin_list.plugins('unittest')) == 2
+
+
+class TestWPPluginConfigRestore:
+
+    def test_restore_add_to_any_config(self):
         pass
 
 
 class TestWPPluginConfig:
 
-    def test_valid_install(self):
+    def test_valid_install(self, wp_site, wp_plugin_list):
+
+        # plugin_name = 'add-to-any'
+        #
+        # # Generic
+        # plugin_config = wp_plugin_list.plugins()[plugin_name]
+        # wp_plugin_config = WPPluginConfig(wp_site, plugin_name, plugin_config)
+        #
+        # assert wp_plugin_config.is_installed is False
+        #
+        # # Specific
+        # plugin_config = wp_plugin_list.plugins('unittest')[plugin_name]
+        # wp_plugin_config = WPPluginConfig(wp_site, plugin_name, plugin_config)
+        #
+        # assert wp_plugin_config.is_installed is False
         pass
 
     def test_valid_uninstall(self):
@@ -25,9 +62,3 @@ class TestWPPluginConfig:
 
 class TestWPPluginConfigExtractor:
     pass
-
-
-class TestWPPluginConfigRestore:
-
-    def test_restore_add_to_any_config(self):
-        pass
