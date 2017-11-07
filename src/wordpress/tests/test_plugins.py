e3150fba580ef433ca88627b5e6c0a9fa8d5f8f8
ebreton <manu@ibimus.com>
2017-11-07 10:58:19 +0100

    added structure for plugin tests

---
+++ src/wordpress/tests/test_plugins.py
@@ -0,0 +1,33 @@
+def test_yaml_include():
+    pass
+
+
+class TestWPPluginList:
+
+    def test_generic_list(self):
+        pass
+
+    def test_list_for_site(self):
+        pass
+
+
+class TestWPPluginConfig:
+
+    def test_valid_install(self):
+        pass
+
+    def test_valid_uninstall(self):
+        pass
+
+    def test_is_activated(self):
+        pass
+
+
+class TestWPPluginConfigExtractor:
+    pass
+
+
+class TestWPPluginConfigRestore:
+
+    def test_restore_add_to_any_config(self):
+        pass
