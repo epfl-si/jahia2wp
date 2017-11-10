e1637f3bdb084bcf294fa9d93e6adeda1712e418
LuluTchab <l.chaboudez@gmail.com>
2017-11-10 14:59:22 +0100

    Latest theme updates (issues 278,279,280,186,126,120,98)

--- data/wp/wp-content/themes/epfl/header.php
+++ data/wp/wp-content/themes/epfl/header.php
@@ -16,14 +16,14 @@
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1" data-header-version="0.26.0">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <script type="text/javascript" src="//www.epfl.ch/js/jquery-epfl.min.js"></script>
-<script type="text/javascript">jQuery.noConflict();</script> 
+<script type="text/javascript">jQuery.noConflict();</script>
 <script type="text/javascript" src="//www.epfl.ch/js/globalnav.js"></script>
 
-<?php readfile("http://www.epfl.ch/templates/fragments/header.sig.html")?>
+<link rel="stylesheet" href="https://static.epfl.ch/v0.26.0/styles/epfl-built.css">
 
 <?php wp_head(); ?>
 </head>
@@ -31,27 +31,50 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'epfl' ); ?></a>
-	
+
 	<div class="header-top wrap">
-  	<?php  readfile("http://www.epfl.ch/templates/fragments/header.fr.html"); ?>
+		<?php
+		/* Configuration */
+		$allowed_langs = array('en', 'fr');
+		$default_lang = 'en';
+		/* If Polylang installed */
+		if(function_exists('pll_current_language'))
+		{
+			/* Get current lang */
+			 $cur_lang = pll_current_language('slug');
+			 /* Check if current lang is supported. If not, use default lang*/
+			 if(!in_array($cur_lang, $allowed_langs)) $cur_lang=$default_lang;
+
+		}
+		else /* Polylang not installed */
+		{
+			$cur_lang = $default_lang;
+		}
+
+
+
+		?>
+  	<header id="epfl-header" class="site-header epfl" data-ajax-header="https://static.epfl.ch/latest/includes/epfl-header.<?php echo $cur_lang; ?>.html"></header>
 	</div><!-- .header-top -->
 
 	<header id="masthead" class="site-header" role="banner">
 
 		<section class="page-tools">
   		<div class="wrap">
-  		
+
+  		<?php if ( !is_front_page() ): ?>
   		<div class="breadcrumb"><?php get_breadcrumb(); ?></div>
-  		
+  		<?php endif; ?>
+
   		<div class="lang">
     		<ul class="language-switcher">
     		  <?php if(function_exists('pll_the_languages'))pll_the_languages(array('hide_if_no_translation'=>1)); ?>
     		</ul>
   		</div>
-  		
+
   		</div><!-- .wrap -->
 		</section><!-- .page-tools -->
-		
+
 		<div class="custom-header">
   		<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
     </div><!-- .custom-header -->
@@ -68,9 +91,9 @@
 
 	<div class="site-content-contain">
 		<div id="content" class="site-content">
-  		
+
   		<?php if ( is_single() || is_page() || is_home() ) : ?>
-  		
+
   		<section class="toolbar wrap">
     		<?php if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) : ?>
     		<div class="social-share">
@@ -79,5 +102,5 @@
     		</div><!-- .social-share -->
     		<?php endif; ?>
   		</section><!-- .toolbox -->
-	  
+
       <?php endif; ?>
