fbc3914f56b1e859f1d5a7ad8ecbe7f57149e7f3
GregLeBarbar <gregory.charmier.epfl.ch>
2017-11-27 10:40:00 +0100

    Improve integration accred plugin

--- src/wordpress/plugins/accred.py
+++ src/wordpress/plugins/accred.py
@@ -5,7 +5,7 @@ from .config import WPPluginConfig
 
 class WPAccredConfig(WPPluginConfig):
 
-    def configure(self):
+    def configure(self, unit_name=None, unit_id=None, **kwargs):
         """
         plugin:epfl_accred:unit is the unit name.
         plugin:epfl_accred:unit_id is the unit id.
@@ -13,10 +13,10 @@ class WPAccredConfig(WPPluginConfig):
         Notice: unit_id will be used in the future.
         """
         # configure options
-        cmd = "option update plugin:epfl_accred:unit {}".format(self.unit_name.upper())
+        cmd = "option update plugin:epfl_accred:unit {}".format(unit_name.upper())
         self.run_wp_cli(cmd)
 
-        cmd = "option add plugin:epfl_accred:unit_id {}".format(self.unit_id)
+        cmd = "option add plugin:epfl_accred:unit_id {}".format(unit_id)
         self.run_wp_cli(cmd)
         logging.info("All accred options added")
 
