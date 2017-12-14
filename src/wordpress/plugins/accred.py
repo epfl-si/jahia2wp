42fa810cc7d3d12694a63c1f522ac2081aa9705c
LuluTchab <l.chaboudez@gmail.com>
2017-12-14 15:32:35 +0100

    Add ability to handle param update (with 'force' parameter)

--- src/wordpress/plugins/accred.py
+++ src/wordpress/plugins/accred.py
@@ -5,7 +5,11 @@ from .config import WPPluginConfig
 
 class WPAccredConfig(WPPluginConfig):
 
-    def configure(self, unit_name=None, unit_id=None, **kwargs):
+    def _option_exists(self, option_name):
+        cmd = "option get {}".format(option_name)
+        return not type(self.run_wp_cli(cmd)) == str
+
+    def configure(self, force, unit_name=None, unit_id=None, **kwargs):
         """
         plugin:epfl_accred:unit is the unit name.
         plugin:epfl_accred:unit_id is the unit id.
@@ -18,9 +22,16 @@ class WPAccredConfig(WPPluginConfig):
             self.run_wp_cli(cmd)
 
         if unit_id is not None:
-            cmd = "option add plugin:epfl_accred:unit_id {}".format(unit_id)
-            self.run_wp_cli(cmd)
-            logging.info("All accred options added")
+            # If option not exists
+            if not self._option_exists("plugin:epfl_accred:unit_id"):
+                cmd = "option add plugin:epfl_accred:unit_id {}".format(unit_id)
+                self.run_wp_cli(cmd)
+                logging.info("All accred options added")
+            else:  # Option exists
+                if force:
+                    cmd = "option update plugin:epfl_accred:unit_id {}".format(unit_id)
+                    self.run_wp_cli(cmd)
+                    logging.info("All accred options updated")
 
         # configure raw plugin
-        super(WPAccredConfig, self).configure()
+        super(WPAccredConfig, self).configure(force)
--- src/wordpress/plugins/polylang.py
+++ src/wordpress/plugins/polylang.py
@@ -1,18 +1,71 @@
 import logging
-
+import json
 from wordpress import WPException
 from .config import WPPluginConfig
 
 
 class WPPolylangConfig(WPPluginConfig):
 
-    def configure(self, languages=None, default=None, **kwargs):
+    def _menu_exists(self, menu_name):
+        """
+        Tells if a menu exists
+
+        Keyword arguments
+        menu_name -- menu name to check if exists
+        """
+        menu_list = json.loads(self.run_wp_cli("menu list --fields=name --format=json"))
+        # if no existing menus
+        if not menu_list:
+            return False
+        # Looping through existing menus
+        for existing_menu in menu_list:
+            if existing_menu['name'] == menu_name:
+                return True
+        return False
+
+    def _language_exists(self, locale):
+        """
+        Tells if a language exists
+
+        Keyword arguments
+        locale -- Local of the language (ex: en_GB, fr_FR, ...)
+        """
+        lang_list = json.loads(self.run_wp_cli("pll lang list --fields=locale --format=json"))
+        # If no language installed
+        if not lang_list:
+            return False
+        # Looping through existing languages
+        for existing_language in lang_list:
+            if existing_language['locale'] == locale:
+                return True
+        return False
+
+    def _get_language_slug(self, locale):
+        """
+        Returns existing language slug based on given locale.
+        Note: all languages have to be installed before calling this function
+
+        Argument keywords
+        locale -- Local of language for which we want the slug.
+        """
+        lang_list = json.loads(self.run_wp_cli("pll lang list --format=json"))
+        # If no language installed
+        if not lang_list:
+            return None
+        # Looping through existing languages
+        for existing_language in lang_list:
+            if existing_language['locale'] == locale:
+                return existing_language['slug']
+        return None
+
+    def configure(self, force, languages=None, default=None, **kwargs):
         """ kwargs:
             - `languages`, array: all languages to install
             - `default`, string: default language should be in `languages`
                 If no default is provided, uses the first item of the array
         """
         # validate input (we keep en_GB instead of en_us to get UK flag, in admin)
+        # FIXME: Parameters are never given so it's always default languages that are taken
         languages = languages or ["fr_FR", "en_GB"]
         default = default or languages[0]
         if default not in languages:
@@ -22,25 +75,32 @@ class WPPolylangConfig(WPPluginConfig):
 
         # adding languages
         for language in languages:
-            is_default = 1 if language == default else 0
-            command = "polylang language add {} {}".format(language, is_default)
-            if not self.run_wp_cli(command):
-                logging.warning("%s - could not install language %s", self.wp_site, language)
-            else:
-                logging.info("%s - installed language %s %s", self.wp_site, language, is_default and "[default]" or "")
+            # If language doesn't already exists
+            if not self._language_exists(language):
+                command = "polylang language add {}".format(language)
+                if not self.run_wp_cli(command):
+                    logging.warning("%s - could not install language %s", self.wp_site, language)
+                else:
+                    logging.info("%s - installed language %s", self.wp_site, language)
+
+        if force:
+            # configure default language
+            default = self._get_language_slug(default)
+            logging.info("%s - setting default language to %s...", self.wp_site, default)
+            self.run_wp_cli("pll option default {}".format(default))
 
         # configure options
-        logging.info("%s - setting polylang options ...", self.wp_site)
+        logging.info("%s - setting options...", self.wp_site)
         self.run_wp_cli("pll option update media_support 0")
-
-        # Configure sync option
-        logging.info("%s - configuring option sync ...", self.wp_site)
         self.run_wp_cli("pll option sync taxonomies")
 
-        # create menus
-        logging.info("%s - creating polylang menu ...", self.wp_site)
-        self.run_wp_cli("pll menu create Main top")
-        self.run_wp_cli("pll menu create footer_nav footer_nav")
+        # create menus if they don't exist
+        logging.info("%s - creating polylang menu...", self.wp_site)
+        if not self._menu_exists("Main"):
+            self.run_wp_cli("pll menu create Main top")
+
+        if not self._menu_exists("footer_nav"):
+            self.run_wp_cli("pll menu create footer_nav footer_nav")
 
         # configure raw plugin
-        super(WPPolylangConfig, self).configure()
+        super(WPPolylangConfig, self).configure(force)
