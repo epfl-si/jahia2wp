d46e045467bcbbd8161464736a7ba0879011c56a
LuluTchab <l.chaboudez@gmail.com>
2017-12-20 13:51:53 +0100

    Update string formatting

--- functional_tests/test_jahia2wp.py
+++ functional_tests/test_jahia2wp.py
@@ -31,42 +31,42 @@ class TestCommandLine:
     # ORDER matters
 
     def test_check_one_fails(self, setup):
-        assert not Utils.run_command('python %s check %s %s'
-                                     % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC))
+        assert not Utils.run_command('python {} check {} {}'.format(
+                                     SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC))
 
     def test_clean_one_fails(self):
-        assert not Utils.run_command('python %s clean %s %s --stop-on-errors'
-                                     % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC))
+        assert not Utils.run_command('python {} clean {} {} --stop-on-errors'.format(
+                                     SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC))
 
     def test_generate_one_success(self):
         expected = "Successfully created new WordPress site at {}".format(SITE_URL_SPECIFIC)
-        assert Utils.run_command('python %s generate %s %s --extra-config=%s'
-                                 % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC, EXTRA_CONFIG_YAML)) == expected
+        assert Utils.run_command('python {} generate {} {} --extra-config={}'.format(
+                                 SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC, EXTRA_CONFIG_YAML)) == expected
 
     def test_backup_full(self):
         expected = "Successfull full backup for {}".format(SITE_URL_SPECIFIC)
-        assert Utils.run_command('python %s backup %s %s'
-                                 % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC)) == expected
+        assert Utils.run_command('python {} backup {} {}'.format(
+                                 SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC)) == expected
 
     def test_backup_incremental(self):
         expected = "Successfull inc backup for {}".format(SITE_URL_SPECIFIC)
-        assert Utils.run_command('python %s backup %s %s'
-                                 % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC)) == expected
+        assert Utils.run_command('python {} backup {} {}'.format(
+                                 SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC)) == expected
 
     def test_list_plugins(self):
         expected = "Plugin list for site '"
-        assert Utils.run_command('python %s list-plugins %s %s --extra-config=%s'
-                                 % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC,
+        assert Utils.run_command('python {} list-plugins {} {} --extra-config={}'.format(
+                                 SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC,
                                  EXTRA_CONFIG_YAML)).startswith(expected)
 
     def test_generate_one_fails(self):
-        assert not Utils.run_command('python %s generate %s %s --extra-config=%s'
-                                     % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC, EXTRA_CONFIG_YAML))
+        assert not Utils.run_command('python {} generate {} {} --extra-config={}'.format(
+                                     SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC, EXTRA_CONFIG_YAML))
 
     def test_check_one_success(self):
         expected = "WordPress site valid and accessible at {}".format(SITE_URL_SPECIFIC)
-        assert Utils.run_command('python %s check %s %s'
-                                 % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC)) == expected
+        assert Utils.run_command('python {} check {} {}'.format(
+                                 SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC)) == expected
 
     def test_wp_admins(self):
         user = WPUser(
@@ -74,12 +74,13 @@ class TestCommandLine:
             Utils.get_mandatory_env(key="WP_ADMIN_EMAIL"),
             role='administrator')
         expected = repr(user)
-        assert Utils.run_command('python %s admins %s %s'
-                                 % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC)) == expected
+        assert Utils.run_command('python {} admins {} {}'.format(
+                                 SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC)) == expected
 
     def test_inventory(self):
 
-        version = Utils.run_command('python %s version %s %s' % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC))
+        version = Utils.run_command('python {} version {} {}'.format(
+                                    SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC))
 
         expected_lines = [
             "path;valid;url;version;db_name;db_user;admins",
@@ -95,5 +96,5 @@ class TestCommandLine:
             assert expected_line in output
 
     def test_clean_one(self):
-        assert Utils.run_command('python %s clean %s %s --stop-on-errors'
-                                 % (SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC))
+        assert Utils.run_command('python {} clean {} {} --stop-on-errors'.format(
+                                 SCRIPT_FILE, OPENSHIFT_ENV, SITE_URL_SPECIFIC))
