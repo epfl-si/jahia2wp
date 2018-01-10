4cf907fd35fed555815c952175d8cbd7400d8301
ebreton <email>
2018-01-10 21:27:10 +0100

    removed bread crumbs

--- src/jahia2wp.py
+++ src/jahia2wp.py
@@ -22,7 +22,6 @@ Usage:
   jahia2wp.py inventory             <path>                          [--debug | --quiet]
   jahia2wp.py extract-plugin-config <wp_env> <wp_url> <output_file> [--debug | --quiet]
   jahia2wp.py list-plugins          <wp_env> <wp_url>               [--debug | --quiet]
-     [--config [--plugin=<PLUGIN_NAME>]] [--extra-config=<YAML_FILE>]
   jahia2wp.py update-plugins        <wp_env> <wp_url>               [--debug | --quiet]
     [--force] [--plugin=<PLUGIN_NAME>]
   jahia2wp.py update-plugins-many   <csv_file>                      [--debug | --quiet]
