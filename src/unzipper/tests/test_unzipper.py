2f09e19572aad266590d0af723041090afec1663
LuluTchab <l.chaboudez@gmail.com>
2018-01-17 14:58:10 +0100

    Change path where to unzip file to avoid 'permission denied' message

--- src/unzipper/tests/test_unzipper.py
+++ src/unzipper/tests/test_unzipper.py
@@ -1,11 +1,12 @@
 import os
-# import shutil
+import shutil
 
 from unzipper.unzip import unzip_one
 
 
 class TestUnzipper(object):
 
+    OUTPUT_DIR = "/tmp"
     CURRENT_DIR = os.path.dirname(__file__)
     TEST_FILE = os.path.join(CURRENT_DIR, "one-site_export_2017-10-11-05-03.zip")
     TEST_JAHIA_SITE = "one-site"
@@ -13,15 +14,15 @@ class TestUnzipper(object):
     def test_unzipped_files_existing(self):
 
         unzip_path = unzip_one(
-            output_dir=self.CURRENT_DIR,
+            output_dir=self.OUTPUT_DIR,
             site_name=self.TEST_JAHIA_SITE,
             zip_file=self.TEST_FILE
         )
 
-        TEST_JAHIA_SITE_PATH = os.path.join(self.CURRENT_DIR, self.TEST_JAHIA_SITE)
+        TEST_JAHIA_SITE_PATH = os.path.join(self.OUTPUT_DIR, self.TEST_JAHIA_SITE)
         unzip_path_expected = os.path.join(TEST_JAHIA_SITE_PATH, self.TEST_JAHIA_SITE)
 
         assert unzip_path == unzip_path_expected
 
-        # clean
-        # shutil.rmtree(TEST_JAHIA_SITE_PATH)
+        # some cleaning
+        shutil.rmtree(TEST_JAHIA_SITE_PATH)
