fbbaa1465af9ae48bc9b9b6ec2fd70b276e7cffe
Luc Venries <luc.venries@epfl.ch>
2018-01-19 10:11:01 +0100

    added source code of the plugin

---
+++ data/wp/wp-content/plugins/EPFL-settings/EPFL-settings.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Plugin Name: EPFL General settings
+ * Description: General settings for all	ow users
+ * Version:     0.1
+ * Author:      wwp-admin@epfl.ch
+ * Text Domain: EPFL-settings
+ */
+
+
+function EPFL_settings_load_plugin_textdomain() {
+  // wp-content/plugins/plugin-name/languages/EPFL-settings-fr_FR.mo
+  load_plugin_textdomain( 'EPFL-settings', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'EPFL_settings_load_plugin_textdomain' );
+
+	
+function EPFL_settings_register_settings() {
+   add_option( 'EPFL_settings_option_name', 'This is my option value.');
+   register_setting( 'EPFL_settings_options_group', 'EPFL_settings_option_name', 'EPFL_settings_callback' );
+   register_setting( 'EPFL_settings_options_group', 'blogname' );
+   register_setting( 'EPFL_settings_options_group', 'blogdescription' );
+   register_setting( 'EPFL_settings_options_group', 'WPLANG' );
+}
+add_action( 'admin_init', 'EPFL_settings_register_settings' );
+
+
+//add menu EPFL settings under settings menu
+function EPFL_settings_register_options_page() {
+  add_options_page('EPFL settings', 'EPFL settings', 'manage_options', 'EPFL_settings', 'EPFL_settings_options_page');
+}
+add_action('admin_menu', 'EPFL_settings_register_options_page');
+
+//config settings page
+function EPFL_settings_options_page()
+{
+?>
+  <div>
+  <?php screen_icon(); ?>
+  <h2><?php echo __("General Settings", 'EPFL-settings');?></h2>
+  <form method="post" action="options.php">
+  <?php settings_fields( 'EPFL_settings_options_group' ); ?>
+  <?php $lang = get_site_option( 'WPLANG' );  $languages = get_available_languages();?>
+
+  <table class="form-table">
+    <tbody><tr>
+      <th scope="row"><label for="blogname"><?php echo __ ("Site Title", 'EPFL-settings');?></label></th>
+      <td><input type="text" id="blogname" name="blogname" value="<?php echo get_option('blogname'); ?>" />
+        <p class="description" id="tagline-description"><?php echo __ ("Acronym (example : IC)", 'EPFL-settings');?></p>
+        <p class="description" id="tagline-description"><a href="<?php admin_url(); ?> admin.php?page=mlang_strings"><?php echo __ ("Site Title translation", 'EPFL-settings');?></a></p></td>
+    </tr>
+    <tr>
+      <th scope="row"><label for="blogdescription"><?php echo __ ("Tagline", 'EPFL-settings');?></label></th>
+      <td><input type="text" id="blogdescription" name="blogdescription" value="<?php echo get_option('blogdescription'); ?>" />
+      <p class="description" id="tagline-description"><?php echo __ ("Explicit name (example : School of Computer and Communication Sciences)", 'EPFL-settings');?></p>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row"><label for="WPLANG"><?php echo __ ("Site administration Language", 'EPFL-settings'    );?></label></th>
+      <td><?php wp_dropdown_languages(array('name' => 'WPLANG', 'id' => 'site-language', 'selected' => $lang, 'languages' => $languages, 'show_available_translations' => false)); ?></td>
+    </tr>
+  </table>
+  <?php  submit_button(); ?>
+  </form>
+  </div>
+<?php
+} ?>
