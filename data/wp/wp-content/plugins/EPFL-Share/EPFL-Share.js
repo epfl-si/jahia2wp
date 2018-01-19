c271eb2e3a7fc2b3618314d697a2cb2c9cd19abf
LuluTchab <l.chaboudez@gmail.com>
2018-01-19 11:21:14 +0100

    EPFL-Share plugin source files

---
+++ data/wp/wp-content/plugins/EPFL-Share/EPFL-Share.js
@@ -0,0 +1,4 @@
+function EPFLSharePopup(e)
+{
+  window.open(e,"popUpWindow","height=400,width=600,left=400,top=100,resizable,scrollbars,toolbar=0,personalbar=0,menubar=no,location=no,directories=no,status");
+}
