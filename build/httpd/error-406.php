fe6629b5df70525cf1f12faca0711a496557e671
Laurent Boatto <me@me>
2018-01-23 10:43:48 +0100

    Custom error page for ModSecurity

---
+++ build/httpd/error-406.php
@@ -0,0 +1,17 @@
+<?php
+
+  // Custom error page for ModSecurity errors.
+
+ $msg = $_SERVER["UNIQUE_ID"];
+?>
+<html>
+<head>
+ <title>Access denied</title>
+ <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+</head>
+<body>
+ <p>You cannot access this page for security reasons. Please contact 1234@epfl.ch and give them this reference number:</p>
+ <p><strong><?php echo $msg?></strong></p>
+</body>
+</html>
+
