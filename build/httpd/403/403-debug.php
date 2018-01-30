2d864f548740a371c5a8d0bb7e36a795010c81a6
Laurent Boatto <me@me>
2018-01-30 10:47:35 +0100

    Html

--- build/httpd/403/403-debug.php
+++ build/httpd/403/403-debug.php
@@ -5,8 +5,7 @@
  ?>
 
  <table style="margin-top:10em">
- <table>
-   <tr><th>Request ID</th><td><?php echo $request_id ?></td></tr>
+ <tr><th>Request ID</th><td><?php echo $request_id ?></td></tr>
    <tr><th>Request URI</th><td><?php echo htmlentities($request_uri) ?></td></tr>
    <tr><th>Remote IP</th><td><?php echo $request_ip ?></td></tr>
    <tr><th>Inside EPFL</th><td><?php echo $is_inside_epfl_string ?></td></tr>
