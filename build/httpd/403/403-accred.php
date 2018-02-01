a92998d218373e74e1b0bb8acc380245f1848f6d
Laurent Boatto <me@me>
2018-02-01 11:18:55 +0100

    403 page for accred

---
+++ build/httpd/403/403-accred.php
@@ -0,0 +1,17 @@
+<p>
+  To access this page you need the right
+  <strong><?php echo $right ?></strong> in unit <strong><?php echo $unit_label ?></strong>.
+</p>
+
+<p>
+  Please contact your accreditor to get access.
+</p>
+
+<ul>
+  <li><a href="https://accred.epfl.ch/cgi-bin/adminsofunite.pl?unite=<?php echo $unit_id ?>">
+  Lookup up who your accreditor is
+  </a></li>
+  <li><a href="https://accreditation.epfl.ch/page-26654-fr.html">
+  Accreditation FAQ (French only)
+  </a></li>
+</ul>
