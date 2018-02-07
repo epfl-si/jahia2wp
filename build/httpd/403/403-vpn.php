7af1f1ae65229e8e6e5b692b600650fab0155f10
Laurent Boatto <me@me>
2018-02-07 08:27:31 +0100

    Support for IPv6 in 403

--- build/httpd/403/403-vpn.php
+++ build/httpd/403/403-vpn.php
@@ -13,6 +13,13 @@
   to connect.
 </p>
 
+<?php if($ip_v == "ipv6") : ?>
+<p>
+  <strong>Important note:</strong> you have an IPv6 address. If you are using
+  VPN, make sure your IPv6 traffic also goes through the VPN, or use IPv4.
+</p>
+<?php endif; ?>
+
 <p>
   If you have troubles using VPN, you can
   <a href="mailto:1234@epfl.ch?Subject=VPN%20access">contact the help desk.</a>
--- build/httpd/403/403.php
+++ build/httpd/403/403.php
@@ -11,12 +11,22 @@
   $request_uri = $_SERVER["REQUEST_URI"];
   $request_ip  = $_SERVER["REMOTE_ADDR"];
 
+  // ip protocol version & regex to check if inside EPFL campus
+  $ip_v = "ipv4";
+  $ip_regex = "/^128\.17(8|9)/";
+
+  if (filter_var($request_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6))
+  {
+    $ip_v = "ipv6";
+    $ip_regex = "/^2001:620:618:/";
+  }
+
   // is the requested page the login or wp-admin?
   $is_login = strpos($request_uri, "wp-login") !== false;
   $is_wp_admin = strpos($request_uri, "wp-admin") !== false;
 
   // is the user's IP inside the EPFL campus?
-  $is_inside_epfl = preg_match('/^128\.17(8|9)/', $request_ip) == 1;
+  $is_inside_epfl = preg_match($ip_regex, $request_ip) == 1;
   $is_inside_epfl_string = $is_inside_epfl ? "Yes" : "No";
 
   // the error type
