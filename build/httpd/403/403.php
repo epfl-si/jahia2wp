71040a16dd4c6bcf10474a1df7788037dccf3e2b
Laurent Boatto <me@me>
2018-02-07 10:56:22 +0100

    Work on accred error type

--- build/httpd/403/403.php
+++ build/httpd/403/403.php
@@ -6,6 +6,9 @@
   // and then includes 403-template.php which in turn includes
   // 403-{$error_type}.php.
 
+  // make sure we send a 403 status code
+  header("HTTP/1.1 403 Forbidden");
+
   // request variables
   $request_id  = $_SERVER["UNIQUE_ID"];
   $request_uri = $_SERVER["REQUEST_URI"];
@@ -29,7 +32,10 @@
   $is_inside_epfl = preg_match($ip_regex, $request_ip) == 1;
   $is_inside_epfl_string = $is_inside_epfl ? "inside EPFL" : "outside EPFL";
 
-  // the error type
+  // the error types supported by this page
+  $error_types = ["default", "vpn", "accred"];
+
+  // the current error type
   $error_type = "default";
 
   // check if the error is that the user is trying to access the
@@ -37,6 +43,15 @@
   if ((($is_login || $is_wp_admin) && !$is_inside_epfl))
   {
     $error_type = "vpn";
+  }
+
+  // the error type can be overridden by a GET parameter,
+  // this is useful for testing and it's used for the
+  // accred error, because it comes from a redirect. We
+  // check that's is a supported error:
+  if (in_array($_GET["error_type"], $error_types))
+  {
+    $error_type = $_GET["error_type"];
   }
 
   include ("403-template.php");
