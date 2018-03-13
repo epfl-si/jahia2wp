848ef656f220c991d632c38c7ca1865ebe40be96
Gil Brechbuhler <gil.brechbuhler@epfl.ch>
2018-03-13 14:15:44 +0100

    adapt file links renaming in menus with new menu handling

--- src/exporter/wp_exporter.py
+++ src/exporter/wp_exporter.py
@@ -286,25 +286,22 @@ class WPExporter:
             # save the new box content
             box.content = str(soup)
 
+    def fix_file_links_in_menu_items(self, menu_item, old_url, new_url):
+        if menu_item.target_is_file():
+                normalized_url = menu_item.target_url.encode('ascii', 'replace').decode('ascii').replace('?', '')
+                normalized_url = normalized_url[normalized_url.rfind("/files"):]
+                if normalized_url == old_url.replace('?', ''):
+                    menu_item.target_url = new_url
+
+    def fix_file_links_in_menus(self, menu, old_url, new_url):
         for lang in self.site.languages:
-            for root_entry_index in range(0, self.site.menus[lang].nb_main_entries()):
-                if self.site.menus[lang].target_is_file(root_entry_index):
-                    normalized_url = self.site.menus[lang].target_url(root_entry_index)
-                    normalized_url = normalized_url.encode('ascii', 'replace').decode('ascii').replace('?', '')
-                    normalized_url = normalized_url[normalized_url.rfind("/files"):]
-                    if normalized_url == old_url.replace('?', ''):
-                        self.site.menus[lang].set_target_url(root_entry_index, new_url)
-                self.fix_file_links_in_submenus(self.site.menus[lang].get_sub_entries(root_entry_index),
-                                                old_url,
-                                                new_url)
+            for root_entry_index, menu_item in enumerate(self.site.menus[lang]):
+                self.fix_file_links_in_menu_items(menu_item, old_url, new_url)
+                self.fix_file_links_in_submenus(menu_item, old_url, new_url)
 
     def fix_file_links_in_submenus(self, menu_item, old_url, new_url):
         for child in menu_item.children:
-            if child.target_is_file():
-                normalized_url = child.target_url.encode('ascii', 'replace').decode('ascii').replace('?', '')
-                normalized_url = normalized_url[normalized_url.rfind("/files"):]
-                if normalized_url == old_url.replace('?', ''):
-                    child.target_url = new_url
+            self.fix_file_links_in_menu_items(child, old_url, new_url)
             self.fix_file_links_in_submenus(child, old_url, new_url)
 
     def fix_page_content_links(self, wp_pages):
