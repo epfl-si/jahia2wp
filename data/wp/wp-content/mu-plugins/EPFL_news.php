10b0694f5ce005e464b0cb00f478094973f83caa
GregLeBarbar <gregory.charmier.epfl.ch>
2018-03-20 13:30:34 +0100

    improve shortcode news

--- data/wp/wp-content/mu-plugins/EPFL_news.php
+++ data/wp/wp-content/mu-plugins/EPFL_news.php
@@ -9,10 +9,29 @@ use utils\Utils as Utils;
 define("NEWS_API_URL", "https://actu.epfl.ch/api/v1/channels/");
 define("NEWS_API_URL_IFRAME", "https://actu.epfl.ch/webservice_iframe/");
 
-/**
- * Build HTML. This template is waiting for Aline templates.
- */
-function epfl_news_build_html($actus): string
+function epfl_news_template_text_only($actus, $stickers): string
+{
+    $actu = '<div>';
+	foreach ($actus->results as $item) {
+		$actu .= '<div style="height: 103px;">';
+		$actu .= '  <div style="display:inline-block;margin-left:5px;">';
+		$actu .= '    <h4>';
+		$actu .= '      <a href="/news/how-the-tuberculosis-bacterium-tricks-the-immune-5/" target="_blank">';
+		$actu .= $item->title;
+		$actu .= '      </a>';
+		$actu .= '    </h4>';
+		$actu .= '    <p>';
+		$actu .= '      <span class="date">' . $item->publish_date . ' -</span>';
+		$actu .= '      <span class="heading" style="display:inline">' . substr($item->subtitle, 0, 40) . '</span>';
+        $actu .= '    </p>';
+		$actu .= '  </div>';
+		$actu .= '</div>';
+    }
+    $actu .= '</div>';
+    return $actu;
+}
+
+function epfl_news_template_fac_with_4_news($actus, $stickers): string
 {
     $actu = '<div>';
 	foreach ($actus->results as $item) {
@@ -35,6 +54,77 @@ function epfl_news_build_html($actus): string
     }
     $actu .= '</div>';
     return $actu;
+}
+
+function epfl_news_template_labo_with_5_news($actus, $stickers): string
+{
+    $actu = '<div>';
+	foreach ($actus->results as $item) {
+		$actu .= '<div style="height: 103px;">';
+		$actu .= '  <a style="float:left;" href="https://actu.epfl.ch/news/' . Utils::get_anchor($item->title) . '">';
+		$actu .= '    <img style="width: 169px;" src="' . $item->visual_url . '" title="">';
+		$actu .= '  </a>';
+		$actu .= '  <div style="display:inline-block;margin-left:5px;">';
+		$actu .= '    <h4>';
+		$actu .= '      <a href="/news/how-the-tuberculosis-bacterium-tricks-the-immune-5/" target="_blank">';
+		$actu .= $item->title;
+		$actu .= '      </a>';
+		$actu .= '    </h4>';
+		$actu .= '    <p>';
+		$actu .= '      <span class="date">' . $item->publish_date . ' -</span>';
+		$actu .= '      <span class="heading" style="display:inline">' . substr($item->subtitle, 0, 40) . '</span>';
+        $actu .= '    </p>';
+		$actu .= '  </div>';
+		$actu .= '</div>';
+    }
+    $actu .= '</div>';
+    return $actu;
+}
+
+function epfl_news_template_labo_with_3_news($actus, $stickers): string
+{
+    $actu = '<div>';
+	foreach ($actus->results as $item) {
+		$actu .= '<div style="height: 103px;">';
+		$actu .= '  <a style="float:left;" href="https://actu.epfl.ch/news/' . Utils::get_anchor($item->title) . '">';
+		$actu .= '    <img style="width: 169px;" src="' . $item->visual_url . '" title="">';
+		$actu .= '  </a>';
+		$actu .= '  <div style="display:inline-block;margin-left:5px;">';
+		$actu .= '    <h4>';
+		$actu .= '      <a href="/news/how-the-tuberculosis-bacterium-tricks-the-immune-5/" target="_blank">';
+		$actu .= $item->title;
+		$actu .= '      </a>';
+		$actu .= '    </h4>';
+		$actu .= '    <p>';
+		$actu .= '      <span class="date">' . $item->publish_date . ' -</span>';
+		$actu .= '      <span class="heading" style="display:inline">' . substr($item->subtitle, 0, 40) . '</span>';
+        $actu .= '    </p>';
+		$actu .= '  </div>';
+		$actu .= '</div>';
+    }
+    $actu .= '</div>';
+    return $actu;
+}
+
+/**
+ * Build HTML. 
+ */
+function epfl_news_build_html($actus, $template, $stickers): string
+{   
+    if ($template === "4") {
+        $html = epfl_news_template_labo_with_3_news($actus, $stickers);
+    } elseif ($template === "8") {
+        $html = epfl_news_template_labo_with_5_news($actus, $stickers);
+    } elseif ($template === "3") {
+        $html = epfl_news_template_fac_with_4_news($actus, $stickers);
+    } elseif ($template === "6") {
+        $html = epfl_news_template_fac_with_3_news($actus, $stickers);
+    } elseif ($template === "2") {
+        $html = epfl_news_template_text_only($actus, $stickers);
+    } else {
+        $html = epfl_news_template_labo_with_3_news($actus, $stickers);
+    }
+    return $html;
 }
 
 /**
@@ -92,7 +182,6 @@ function epfl_news_build_api_url(
     string $themes
     ): string
 {
-
     // returns the number of news according to the template
     $limit = epfl_news_get_limit($template);
 
@@ -124,13 +213,18 @@ function epfl_news_check_parameters(string $channel, string $lang): bool {
 /**
  * Main function of shortcode
  */
-function epfl_news_process_shortcode(array $atts, string $content = '', string $tag): string {
+function epfl_news_process_shortcode(
+    array $atts, 
+    string $content = '', 
+    string $tag
+    ): string {
 
         // extract shortcode paramepfl_newseter
         $atts = extract(shortcode_atts(array(
                 'channel' => '',
                 'lang' => '',
                 'template' => '',
+                'stickers' => '',
                 'category' => '',
                 'themes' => '',
         ), $atts, $tag));
@@ -138,6 +232,8 @@ function epfl_news_process_shortcode(array $atts, string $content = '', string $
         if (epfl_news_check_parameters($channel, $lang) == FALSE) {
             return "";
         }
+        
+        $stickers = $stickers == 'yes';
 
         // iframe template
         if ($template === "10") {
@@ -153,7 +249,7 @@ function epfl_news_process_shortcode(array $atts, string $content = '', string $
         );
 
         $actus = Utils::get_items($url);
-        return epfl_news_build_html($actus);
+        return epfl_news_build_html($actus, $template, $stickers);
 }
 
 // define the shortcode
