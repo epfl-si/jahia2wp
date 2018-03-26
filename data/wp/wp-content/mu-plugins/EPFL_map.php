bc77a47d3954e42af7ff71f6c442bbd752f9becb
GregLeBarbar <gregory.charmier.epfl.ch>
2018-03-26 06:19:17 +0200

    Improve map shortcode

--- data/wp/wp-content/mu-plugins/EPFL_map.php
+++ data/wp/wp-content/mu-plugins/EPFL_map.php
@@ -7,14 +7,15 @@
  * @copyright: Copyright (c) 2017 Ecole Polytechnique Federale de Lausanne, Switzerland
  */
 
-declare(strict_types=1);
+declare( strict_types = 1 );
 
 /**
  * Helper to debug the code
+ * @param $var: variable to display
  */
-function debug($var) {
+function epfl_map_debug( $var ) {
     print "<pre>";
-    var_dump($var);
+    var_dump( $var );
     print "</pre>";
 }
 
@@ -25,18 +26,15 @@ function debug($var) {
  * @param $height: height of the map iframe
  * @param $query: query example: the office, the person
  * @param $lang: language
+ * @return string html of map iframe
  */
-function epfl_map_build_html(
-    string $width,
-    string $height,
-    string $query,
-    string $lang): string
+function epfl_map_build_html( string $width, string $height, string $query, string $lang ): string
 {
-    $html = '<iframe frameborder="0" ';
-    $html .= 'width="' . $width . '"';
-    $html .= 'height="' . $height . '"';
-    $html .= '"scrolling="no" src="https://plan.epfl.ch/iframe/?q=' . $query;
-    $html .= '&amp;lang=' . $lang . '&amp;map_zoom=10"></iframe>';
+    $html  = '<iframe frameborder="0" ';
+    $html .= 'width="' . esc_attr($width) . '" ';
+    $html .= 'height="' . esc_attr($height) . '" ';
+    $html .= 'scrolling="no" src="https://plan.epfl.ch/iframe/?q=' . esc_attr($query);
+    $html .= '&amp;lang=' . esc_attr($lang) . '&amp;map_zoom=10"></iframe>';
     return $html;
 }
 
@@ -49,8 +47,9 @@ function epfl_map_build_html(
  * @param $height: height of the map iframe
  * @param $query: query example: the office, the person
  * @param $lang: language
+ * @return True if all parameters are populated
  */
-function check_parameters(
+function epfl_map_check_parameters(
     string $width,
     string $height,
     string $query,
@@ -59,24 +58,36 @@ function check_parameters(
     return $width !== "" && $height !== "" && $query !== "" && $lang !== "";
 }
 
-function epfl_map_process_shortcode(
-    $attributes,
-    string $content = null): string
+/**
+ * Execute the shortcode
+ *
+ * @attributes: array of all input parameters
+ * @content: the content of the shortcode. In our case the content is empty
+ * @return html of shortcode
+ */
+function epfl_map_process_shortcode( $attributes, string $content = null ): string
 {
-    extract(shortcode_atts(array(
-        'width' => '',
+    // get parameters
+    $atts = shortcode_atts(array(
+        'width'  => '',
         'height' => '',
-        'query' => '',
-        'lang' => '',
-    ), $attributes));
+        'query'  => '',
+        'lang'   => '',
+    ), $attributes);
 
-    if (check_parameters($width, $height, $query, $lang) == FALSE) {
+    // sanitize parameters
+    $width  = sanitize_text_field($atts['width']);
+    $height = sanitize_text_field($atts['height']);
+    $query  = sanitize_text_field($atts['query']);
+    $lang   = sanitize_text_field($atts['lang']);
+
+    // check parameters
+    if ( epfl_map_check_parameters($width, $height, $query, $lang) == FALSE ) {
         return "";
     }
-
-    return epfl_map_build_html($width, $height, $query, $lang);
+    return epfl_map_build_html( $width, $height, $query, $lang );
 }
 
-add_shortcode('epfl_map', 'epfl_map_process_shortcode');
+add_shortcode( 'epfl_map', 'epfl_map_process_shortcode' );
 
 ?>
