9ee1f41f919e0bb8db5d398859474c01d1afa36c
GregLeBarbar <GregLeBarbar@users.noreply.github.com>
2018-04-05 17:28:56 +0200

    Add map and scheduler shortcode to generator (#348)

--- src/wordpress/generator.py
+++ src/wordpress/generator.py
@@ -365,6 +365,8 @@ class WPGenerator:
         WPMuPluginConfig(self.wp_site, "epfl-functions.php").install()
         WPMuPluginConfig(self.wp_site, "EPFL-SC-infoscience.php").install()
         WPMuPluginConfig(self.wp_site, "EPFL_custom_editor_menu.php").install()
+        WPMuPluginConfig(self.wp_site, "EPFL-map.php").install()
+        WPMuPluginConfig(self.wp_site, "EPFL-scheduler.php").install()
 
         if self.wp_config.installs_locked:
             WPMuPluginConfig(self.wp_site, "EPFL_installs_locked.php").install()
