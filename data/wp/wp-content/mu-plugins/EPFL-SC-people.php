1c7ddc625b636a276094d8a5b4184f44c5fba7b3
GregLeBarbar <GregLeBarbar@users.noreply.github.com>
2018-04-06 10:25:25 +0200

    people shortcode (#345)
    * people shortcode
    
    * people shortcode
    
    * Add people MU-Plugin to generator
    
    * fix GET parameter template value
    
    * improve function name
    
    * Improve people shortcode
    
    * fix
    
    * Fix
    
    * typo
    
    * dont use file_get_contents

---
+++ data/wp/wp-content/mu-plugins/EPFL-SC-people.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Plugin Name: EPFL People shortcode
+ * Plugin URI: https://github.com/epfl-idevelop/EPFL-WP-SC-People
+ * Description: provides a shortcode to display results from People
+ * Version: 1.1
+ * Author: Emmanuel JAEP
+ * Author URI: https://people.epfl.ch/emmanuel.jaep?lang=en
+ * Contributors: LuluTchab, GregLeBarbar
+ * License: Copyright (c) 2017 Ecole Polytechnique Federale de Lausanne, Switzerland
+ **/
+
+function epfl_people_log( $message ) {
+    if ( WP_DEBUG === true ) {
+        if ( is_array( $message ) || is_object( $message ) ) {
+            error_log( print_r( $message, true ) );
+        } else {
+            error_log( $message );
+        }
+    }
+}
+
+function epfl_people_url_exists( $url )
+{
+    $handle = curl_init( $url );
+    curl_setopt( $handle, CURLOPT_RETURNTRANSFER, TRUE );
+
+    $response = curl_exec( $handle );
+    $httpCode = curl_getinfo( $handle, CURLINFO_HTTP_CODE );
+
+    if ( $httpCode >= 200 && $httpCode <= 400 ) {
+        return true;
+    } else {
+        return false;
+    }
+    curl_close( $handle );
+}
+
+function epfl_people_process_shortcode( $attributes, $content = null )
+{
+    $attributes = shortcode_atts( array(
+        'url' => ''
+    ), $attributes );
+
+    // Sanitize parameter
+    $url = sanitize_text_field( $attributes['url'] );
+
+    // Check if the result is already in cache
+    $result = wp_cache_get( $url, 'epfl_people' );
+
+    if ( false === $result ){
+
+        // Make sure the content is actually coming from the people pages and does exist
+        if ( ( strcasecmp( parse_url( $url, PHP_URL_HOST ), 'people.epfl.ch' ) == 0 or strcasecmp( parse_url( $url, PHP_URL_HOST ), 'test-people.epfl.ch' ) == 0 ) && epfl_people_url_exists( $url ) ) {
+
+            // Get the content of the page
+            $response = wp_remote_get( $url );
+            $page = wp_remote_retrieve_body( $response );
+
+            // cache the result
+            wp_cache_set( $url, $page, 'epfl_people' );
+
+            // return the page
+            return $page;
+        } else {
+            $error = new WP_Error( 'not found', 'The url passed is not part of people or is not found', $url );
+            epfl_people_log( $error );
+        }
+    } else {
+        // Use cache
+        return $result;
+    }
+}
+
+add_shortcode('epfl_people', 'epfl_people_process_shortcode');
+
+?>
