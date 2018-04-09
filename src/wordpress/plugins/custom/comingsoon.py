21a7e110f011322f3daa13c05804e9ab196a87be
LuluTchab <l.chaboudez@gmail.com>
2018-04-09 16:04:29 +0200

    Customize login URL for website

---
+++ src/wordpress/plugins/custom/comingsoon.py
@@ -0,0 +1,36 @@
+import logging
+import json
+from wordpress import WPException
+from wordpress.plugins.config import WPPluginConfig
+import settings
+
+
+class WPComingSoonConfig(WPPluginConfig):
+
+    def configure(self, force, **kwargs):
+        """ kwargs:
+            - force -- True|False to tell if we have to erase configuration if already exists
+        """
+
+        # configure options
+        logging.info("{} - ComingSoon - Setting options...".format(self.wp_site))
+
+        # Loading current configuration
+        option = json.loads(self.run_wp_cli('option get seed_csp4_settings_content --format=json'))
+
+        option['logo'] = 'https://mediacom.epfl.ch/files/content/sites/mediacom/files/EPFL-Logo.jpg'
+        option['headline'] = 'Something new is coming...'
+        option['description'] = '&nbsp;<div class="footer-content"><nav class="footer-navigation" role="navigation"> \
+</nav><p class="site-admin" style="position:absolute;bottom:0;width:50%;text-align:right;"><span style="font-size:10pt; \
+font-family:arial,helvetica,sans-serif;"><a href="{}/wp-admin/">Connexion / Login</a></span></p></div>'.format(
+            self.wp_site.url
+        )
+        option['footer_credit'] = '1'
+        # If we have to force update, we display "ComingSoon" screen
+        if force:
+            option['status'] = '1'
+
+        self.run_wp_cli("option update seed_csp4_settings_content --format=json ", pipe_input=json.dumps(option))
+
+        # configure raw plugin
+        super(WPComingSoonConfig, self).configure(force)
