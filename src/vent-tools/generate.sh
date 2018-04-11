8f06c40ff73b6db150b15f7cc1e592fe63cc6552
Hugo M. Muriel Arriaran <marcelomuriel@gmail.com>
2018-04-11 10:05:40 +0200

    minor change: removed an entry

--- src/vent-tools/generate.sh
+++ src/vent-tools/generate.sh
@@ -7,7 +7,6 @@ python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/labs --a
 python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/labs/dcsl --admin-password=jahia2wp --extra-config=generate.yml 
 python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/labs/nal --admin-password=jahia2wp --extra-config=generate.yml 
 python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/publications --admin-password=jahia2wp --extra-config=generate.yml 
-python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/publications/dcsl --admin-password=jahia2wp --extra-config=generate.yml 
 
 python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/inside --admin-password=jahia2wp --extra-config=generate.yml 
 python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/inside/students --admin-password=jahia2wp --extra-config=generate.yml 
