306c4c4f3fc3db66523928483fa03094a8515a64
Hugo M. Muriel Arriaran <marcelomuriel@gmail.com>
2018-04-16 15:50:21 +0200

    updated export tools with new cases and multiple export (export-many) after single export broke

--- src/vent-tools/export.sh
+++
@@ -1,2 +0,0 @@
-python jahia2wp.py export dcsl http://jahia2wp-httpd/dcsl idevelop --to-wordpress --admin-password=jahia2wp
-python jahia2wp.py export sac http://jahia2wp-httpd/sac idevelop --to-wordpress --admin-password=jahia2wp
