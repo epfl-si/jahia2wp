ce97c4ef9f04cb2bd82ff32de58e90e1067dcd83
Hugo M. Muriel Arriaran <marcelomuriel@gmail.com>
2018-04-24 14:30:52 +0200

    Added functionality to also clean the menues to avoid having duplicates in the custom menu entries (e.g. legal notice)

--- src/vent-tools/del-posts.sh
+++ src/vent-tools/del-posts.sh
@@ -13,4 +13,14 @@ do
 	else
 		echo "No posts for $path"
 	fi
+	# Delete any remaining menu entries
+	menu_list=$(wp menu list --fields=term_id --format=ids --path="${mpath}/${path}")
+	for menu in $menu_list
+	do
+		db_ids=$(wp menu item list $menu --fields=db_id --format=ids --path="${mpath}/${path}")
+		if [ -n "$db_ids" ]; then
+			echo "menu entries from menu $menu to delete: $db_ids";
+			wp menu item delete $db_ids --path="${mpath}/${path}";
+		fi
+	done
 done
