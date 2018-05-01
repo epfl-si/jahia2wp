077aa0b2d1f29765b10fc6dbe729114fe7fa11d3
Laurent Boatto <me@me>
2018-05-01 11:15:26 +0200

    Doc

--- src/vent-tools/generate.sh
+++ src/vent-tools/generate.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+#
+# Generate the sites tree from the source of truth
+#
+# For now it's hardcoded, but in the future it will take a CSV file as a parameter.
+
 python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml 
-python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/education --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml 
-wp pll lang create Français fr fr_FR --path=/srv/hmuriel/jahia2wp-httpd/htdocs/epfl/education
-python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml 
+#python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/education --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml
+python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml
 python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/labs --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml 
 python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/labs/dcsl --admin-password=jahia2wp --extra-config=vent-tools/generate.yml 
-python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/labs/nal --admin-password=jahia2wp --extra-config=vent-tools/generate.yml 
+#python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/labs/nal --admin-password=jahia2wp --extra-config=vent-tools/generate.yml
 python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/epfl/research/publications --admin-password=jahia2wp --extra-config=vent-tools/generate.yml 
 
-python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/inside --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml 
-python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/inside/students --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml 
+#python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/inside --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml
+#python jahia2wp.py generate $WP_ENV http://jahia2wp-httpd/inside/students --admin-password=jahia2wp --extra-config=vent-tools/generate-enfr.yml
 
 # Move the accred and tequila plugins to let for local connections
 find /srv/$WP_ENV/jahia2wp-httpd/htdocs/ \( -iname "accred" -o -iname "tequila" \) -exec mv {} {}.bak \;
