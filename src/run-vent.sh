ae420c2cc26874b2f6adbfa4bf107606fb98a31c
Hugo M. Muriel Arriaran <marcelomuriel@gmail.com>
2018-05-02 10:15:04 +0200

    renamed

--- src/run-vent.sh
+++
@@ -1,5 +0,0 @@
-bash ./vent-tools/del-posts.sh
-
-export ROOT_WP_DEST=/srv/lboatto/jahia2wp-httpd/htdocs/epfl/
-
-PYTHONIOENCODING="utf-8" python jahia2wp.py migrate-urls ../data/csv/ventilation-local.csv $WP_ENV --root_wp_dest=$ROOT_WP_DEST
