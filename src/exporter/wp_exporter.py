ba47b46682edba2d48e494693551aed3e871c584
GregLeBarbar <gregory.charmier.epfl.ch>
2018-05-18 11:05:22 +0200

    fix indentation

--- src/exporter/wp_exporter.py
+++ src/exporter/wp_exporter.py
@@ -622,12 +622,12 @@ class WPExporter:
                     if not box.is_shortcode():
                         contents[lang] += '<div class="{}">'.format(box.type + "Box")
 
-                    if box.title:
-                        if WPUtils.is_html(box.title):
-                            contents[lang] += '<h3>{0}</h3>'.format(box.title)
-                        else:
-                            slug = slugify(box.title)
-                            contents[lang] += '<h3 id="{0}">{0}</h3>'.format(slug, box.title)
+                        if box.title:
+                            if WPUtils.is_html(box.title):
+                                contents[lang] += '<h3>{0}</h3>'.format(box.title)
+                            else:
+                                slug = slugify(box.title)
+                                contents[lang] += '<h3 id="{0}">{0}</h3>'.format(slug, box.title)
 
                     # in the parser we can't know the current language.
                     # we assign a string that we replace with the current language
