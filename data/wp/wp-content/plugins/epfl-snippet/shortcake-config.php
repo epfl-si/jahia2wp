4685b8afaab58e1928949e3b9253e4815fcb03d0
GregLeBarbar <gregory.charmier.epfl.ch>
2018-05-25 10:26:32 +0200

    indentation

--- data/wp/wp-content/plugins/epfl-snippet/shortcake-config.php
+++ data/wp/wp-content/plugins/epfl-snippet/shortcake-config.php
@@ -12,37 +12,37 @@ Class ShortCakeSnippetConfig
                 'label' => __('Add Snippet shortcode', 'epfl-snippet'),
                 'listItemImage' => '',
                 'attrs'         => array(
-                        array(
-                            'label'         => '<h3>' . esc_html__('URL', 'epfl-snippet') . '</h3>',
-                            'attr'          => 'url',
-                            'type'          => 'text',
-                            'description'   => esc_html__('URL', 'epfl-snippet'),
-                        ),
-                        array(
-                            'label'         => '<h3>' . esc_html__('Title', 'epfl-snippet') . '</h3>',
-                            'attr'          => 'title',
-                            'type'          => 'text',
-                            'description'   => esc_html__('Title', 'epfl-snippet'),
-                        ),
-                        array(
-                            'label'         => '<h3>' . esc_html__('SubTitle', 'epfl-snippet') . '</h3>',
-                            'attr'          => 'subtitle',
-                            'type'          => 'text',
-                            'description'   => esc_html__('SubTitle', 'epfl-snippet'),
-                        ),
-                        array(
-                            'label'         => '<h3>' . esc_html__('Image', 'epfl-snippet') . '</h3>',
-                            'attr'          => 'image',
-                            'type'          => 'text',
-                            'description'   => esc_html__('Image', 'epfl-snippet'),
-                        ),
-                        array(
-                            'label'         => '<h3>' . esc_html__('Big Image', 'epfl-snippet') . '</h3>',
-                            'attr'          => 'big_image',
-                            'type'          => 'text',
-                            'description'   => esc_html__('Big Image', 'epfl-snippet'),
-                        ),
+                    array(
+                        'label'         => '<h3>' . esc_html__('URL', 'epfl-snippet') . '</h3>',
+                        'attr'          => 'url',
+                        'type'          => 'text',
+                        'description'   => esc_html__('URL', 'epfl-snippet'),
                     ),
+                    array(
+                        'label'         => '<h3>' . esc_html__('Title', 'epfl-snippet') . '</h3>',
+                        'attr'          => 'title',
+                        'type'          => 'text',
+                        'description'   => esc_html__('Title', 'epfl-snippet'),
+                    ),
+                    array(
+                        'label'         => '<h3>' . esc_html__('SubTitle', 'epfl-snippet') . '</h3>',
+                        'attr'          => 'subtitle',
+                        'type'          => 'text',
+                        'description'   => esc_html__('SubTitle', 'epfl-snippet'),
+                    ),
+                    array(
+                        'label'         => '<h3>' . esc_html__('Image', 'epfl-snippet') . '</h3>',
+                        'attr'          => 'image',
+                        'type'          => 'text',
+                        'description'   => esc_html__('Image', 'epfl-snippet'),
+                    ),
+                    array(
+                        'label'         => '<h3>' . esc_html__('Big Image', 'epfl-snippet') . '</h3>',
+                        'attr'          => 'big_image',
+                        'type'          => 'text',
+                        'description'   => esc_html__('Big Image', 'epfl-snippet'),
+                    ),
+                ),
                 'inner_content' => array(
                    'label'        => '<h3>' . esc_html__( 'Content of snippet', 'epfl-snippet' ) . '</h3>',
                    'description'  => esc_html__('You can enter text to display above', 'epfl-snippet'),
