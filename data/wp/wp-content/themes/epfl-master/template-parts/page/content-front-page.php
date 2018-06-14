220f0c43156b5e6124d456cd4c40025db37c186b
Aline <aline.keller@gmail.com>
2018-06-14 16:00:04 +0200

    [themes/epfl-master] Add styles for faculty homepages

--- data/wp/wp-content/themes/epfl-master/template-parts/page/content-front-page.php
+++ data/wp/wp-content/themes/epfl-master/template-parts/page/content-front-page.php
@@ -10,20 +10,14 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('homepage-content'); ?> >
-  
-  <div class="panel-content">
-		<div class="wrap">
-			<div class="entry-content">
-				<?php
-					/* translators: %s: Name of current post */
-					the_content( sprintf(
-						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'epfl' ),
-						get_the_title()
-					) );
-				?>
-			</div><!-- .entry-content -->
-
-		</div><!-- .wrap -->
-	</div><!-- .panel-content -->
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'epfl' ),
+				get_the_title()
+			) );
+		?>
+	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
