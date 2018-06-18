65cfa1a1cc4322ce7741bf98bcddc25d8f8813c6
Laurent Boatto <me@me>
2018-06-18 08:54:25 +0200

    Zip plugin

--- bin/zip-plugin.sh
+++ bin/zip-plugin.sh
@@ -13,14 +13,14 @@ then
   exit
 fi
 
-# paths we will use (use $PWD to have the absolute paths because we will do a 'cd' later
+# paths we will use (use $PWD to have the absolute paths because we will do a 'cd' later)
 ZIP_FILE=$PWD/../data/plugins/generic/$1/v1/$1.zip
 PLUGIN_DIR=$PWD/../data/wp/wp-content/plugins/$1
 
 # check if the plugin directory exists
 if [ ! -d ${PLUGIN_DIR} ]
 then
- echo "ERROR: directory ../data/wp/wp-content/plugins/$1 doesn't exist"
+ echo "ERROR: plugin directory ${PLUGIN_DIR} doesn't exist"
  exit
 fi
 
