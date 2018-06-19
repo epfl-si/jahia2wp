faf443e611bee4867585c802c1c99507ada7c0ae
Laurent Boatto <me@me>
2018-06-19 10:49:57 +0200

    epfl-people-2018 shortcode

---
+++ data/wp/wp-content/plugins/epfl-people-2018/utils.php
@@ -0,0 +1,31 @@
+<?php
+
+Class PeopleUtils
+{
+    public static function debug($var) {
+        print "<pre>";
+        var_dump($var);
+        print "</pre>";
+    }
+
+    /**
+     * Call API
+     * @param url  : the fetchable url
+     * @param args : array('timeout' => 10), see https://codex.wordpress.org/Function_Reference/wp_remote_get
+     * @return decoded JSON data
+     */
+    public static function get_items(string $url) {
+    	
+        $response = wp_remote_get($url);
+
+        if (is_array($response)) {
+        	
+                $header = $response['headers']; // array of http header lines
+                $data = $response['body']; // use the content
+                
+                return json_decode($data);     
+        }
+    }
+}
+
+?>
