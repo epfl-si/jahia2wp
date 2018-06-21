2bfb3956a72fa4ef28e45a8909987da29da421a0
Julien Delasoie <julien.delasoie@epfl.ch>
2018-06-21 14:18:11 +0200

    Fix article short printing

--- data/wp/wp-content/plugins/epfl-infoscience-search/templates/journal_articles_short.php
+++ data/wp/wp-content/plugins/epfl-infoscience-search/templates/journal_articles_short.php
@@ -1,35 +1,34 @@
-<div>
-    <?php include $template_base_path . 'common/authors.php' ?>
+<?php include $template_base_path . 'common/authors.php' ?>
 
-    <?php 
-        if ($publication['author']) {
-            echo "<span> : </span>";
-        }
+<?php 
+    if ($publication['author']) {
+        echo "<span> : </span>";
+    }
 
-        if ($publication['title']) {
-            echo "<span><strong>" . $publication['title'][0] . "</strong></span>";
-            
-            echo "<span>";
-            if ($publication['journal'][0]['publisher']) {
-                echo "; ";
-            } else {
-                echo ". ";
-            }
-            echo "</span>";
+    if ($publication['title']) {
+        echo "<span><strong>" . $publication['title'][0] . "</strong></span>";
+    }
+        
+    echo "<span>";
+    if ($publication['journal'][0]['publisher']) {
+        echo "; ";
+    } else {
+        echo ". ";
+    }
+    echo "</span>";
 
-            if ($publication['journal'][0]['publisher']) {
-                echo "<span><i>" . $publication['journal'][0]['publisher'] . "</i></span>";
-                echo "<span>. </span>";
-            }
+    if ($publication['journal'][0]['publisher']) {
+        echo "<span><i>" . $publication['journal'][0]['publisher'] . "</i></span>";
+        echo "<span>. </span>";
+    }
 
-            if ($publication['publication_year']) {
-                echo "<span><i>" . $publication['publication_year'][0] . "</i></span>";
-                echo "<span>. </span>";
-            }
+    if ($publication['publication_year']) {
+        echo "<span><i>" . $publication['publication_year'][0] . "</i></span>";
+        echo "<span>. </span>";
+    }
 
-            if ($publication['doi']) {
-                echo "<span>DOI : " . $publication['doi'][0] . "</span>";
-            }
-        }
-    ?>
-</div>
+    if ($publication['doi']) {
+        echo "<span>DOI : " . $publication['doi'][0] . "</span>";
+    }
+
+?>
