cde219fb451b1bb5fc8199f4072ca68849cfdc31
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-06-21 16:17:58 +0200

    Flesh out the Item class + misc.
    * Custom exception class for failed expectations regarding the XML structure
    
    * all() and insert() named constructors
    
    * Mutation accessors: Item.insert() and Item().delete(); getters and
      setters for the WordPress integer ID

--- src/wxr-tools/wxr-ventilate.py
+++ src/wxr-tools/wxr-ventilate.py
@@ -23,22 +23,96 @@ Options:
 from docopt import docopt
 import lxml.etree
 
+class WXMLError(Exception):
+    pass
 
 class Ventilator:
     def __init__(self, file):
         self.etree = lxml.etree.parse(file)
 
-    def ventilate(self):
+    def ventilate(self, flags):
         return self.etree
 
 def to_string(what):
-    if isinstance(what, lxml.etree._ElementTree):
+    if (isinstance(what, lxml.etree._Element) or
+        isinstance(what, lxml.etree._ElementTree)):
         return lxml.etree.tostring(what, encoding='utf-8').decode('utf-8')
     else:
         return str(what)
 
+WP_NSMAP = {
+    'content': 'http://purl.org/rss/1.0/modules/content/',
+    'dc'     : 'http://purl.org/dc/elements/1.1/',
+    'wp'     : 'http://wordpress.org/export/1.2/',
+    'wfw'    : 'http://wellformedweb.org/CommentAPI/',
+    'excerpt': 'http://wordpress.org/export/1.2/excerpt/'
+}
+
+class Item:
+    """An <item> in the XML document.
+
+    An item represents a WordPress post of any type, including attachments,
+    Polylang translation tables and even weirder instances (but see
+    the all_pages class method).
+    """
+    def __init__(self, etree_elt):
+        self._elt = etree_elt
+
+    @classmethod
+    def all(cls, etree):
+        return [cls(elt) for elt in etree.xpath("//item")]
+
+    @classmethod
+    def insert(cls, etree):
+        """Create and return an empty <item> element."""
+        new_elt = lxml.etree.Element("item")
+        existing_items = etree.xpath("//item")
+        if existing_items:
+            existing_items[0].addprevious(new_elt)
+        else:
+            etree.xpath("/rss/channel").append(new_elt)
+        return cls(new_elt)
+
+    def delete(self):
+        self._elt.getparent().remove(self._elt)
+        self._elt = 'DELETED'
+
+    def __id_node(self):
+        nodes = self._elt.xpath("wp:post_id", namespaces=WP_NSMAP)
+        if len(nodes) == 1:
+            return nodes[0]
+        elif len(nodes) == 0:
+            new_post_id = lxml.etree.Element("{%s}post_id" % WP_NSMAP["wp"])
+            self._elt.append(new_post_id)
+            return new_post_id
+        else:
+            raise WXMLError("%d wp:post_id's in item!" % len(nodes))
+
+    @property
+    def id(self):
+        id_text = self.__id_node().text
+        return None if id_text is None else int(id_text)
+
+    @id.setter
+    def id(self, new_id):
+        self.__id_node().text = str(new_id)
+
+    def __repr__(self):
+        return '[Item %s]' % to_string(self._elt)
+
+def demo():
+    """For running in ipython - Not used in production"""
+    import subprocess
+    global srcdir, v, i
+    srcdir = (subprocess.run("git rev-parse --show-toplevel", stdout=subprocess.PIPE, shell=True).stdout)[:-1]
+    v = Ventilator("%s/wxr/cri.xml" % srcdir.decode("utf-8"))
+    i = Item.insert(v.etree)
+    print(repr(i.id))
+    i.id = 5
+    print(repr(i.id))
+    print(to_string(i))
 
 if __name__ == '__main__':
     args = docopt(__doc__)
     v = Ventilator(args["<input_xml_file>"])
-    print(to_string(v.ventilate()))
+    print(to_string(v.ventilate(args)))
