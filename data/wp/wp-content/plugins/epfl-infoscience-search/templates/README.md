65eb0035ffdc6ca84b14d1c0e68ddda52194afcb
Julien Delasoie <julien.delasoie@epfl.ch>
2018-06-21 14:58:43 +0200

    Add article detailed

---
+++ data/wp/wp-content/plugins/epfl-infoscience-search/templates/README.md
@@ -0,0 +1,5 @@
+# what not respected at the moment from infoscience-export
+
+## not advanced show
+- short format show only direct info
+- detail show all (like volume, number, page)
--- data/wp/wp-content/plugins/epfl-infoscience-search/templates/journal_articles_detailed.php
+++ data/wp/wp-content/plugins/epfl-infoscience-search/templates/journal_articles_detailed.php
@@ -1,3 +1,47 @@
-<div>
-   HELLO DETAILED WORLD !
-</div>
+<?php
+    if ($publication['title']) {
+        echo '<h3 class="infoscience_title">' . $publication['title'][0] .'</h3>';
+    }
+
+    if ($publication['author']) {
+        echo '<p class="infoscience_authors">';
+        include $template_base_path . 'common/authors.php';
+        echo '</p>';
+    }
+
+    echo '<p class="infoscience_host">';
+
+    if ($publication['journal'][0]['publisher']) {
+        echo "<span><i>" . $publication['journal'][0]['publisher'] . "</i>. </span>";
+    }
+
+    if ($publication['publication_year']) {
+        echo "<span>" . $publication['publication_year'][0] . ". </span>";
+    }
+
+    if ($publication['doi']) {
+        echo "<p>DOI : " . $publication['doi'][0] . ".</p>";
+    }
+
+    if ($publication['journal'][0]['volume']) {
+        echo '<span>' . __('Vol.', 'epfl_infoscience') . ' ' . $publication['journal'][0]['volume'] .'</span> ';
+        if ($publication['journal'][0]['number'] || $publication['journal'][0]['page']) {
+            echo '<span>, </span>';
+        } else {
+            echo '<span>. </span>';
+        }
+    }
+
+    if ($publication['journal'][0]['number']) {
+        echo '<span>' . __('num.', 'epfl_infoscience') . ' ' . $publication['journal'][0]['number'] .'</span> ';
+        if ($publication['journal'][0]['page']) {
+            echo '<span>, </span>';
+        } else {
+            echo '<span>. </span>';
+        }
+    }
+
+    if ($publication['journal'][0]['page']) {
+        echo '<span>' . __('p.', 'epfl_infoscience') . ' ' . $publication['journal'][0]['page'] .'.</span> ';
+    }
+?>
