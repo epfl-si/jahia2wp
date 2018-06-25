529b3b8c63d58c4a2949f38b203efe9b793175dc
Julien Delasoie <julien.delasoie@epfl.ch>
2018-06-25 13:44:58 +0200

    Add patents templates

---
+++ data/wp/wp-content/plugins/epfl-infoscience-search/templates/patents_short.php
@@ -0,0 +1,57 @@
+<?php include $template_base_path . 'common/authors.php' ?>
+
+<?php 
+    if ($publication['author']) {
+        echo "<span> ; </span>";
+    }
+
+    if ($publication['corporate_name']) {
+        echo "<span>" . $publication['corporate_name'][0] . "</span>";
+        if ($publication['company_name']) {
+            echo "<span> / </span>";
+        } else {
+            echo "<span>: </span>";    
+        }
+        echo "<span>. </span>";
+    }
+
+    if ($publication['company_name']) {
+        echo "<span>" . $publication['company_name'][0] . "</span>";
+        echo "<span>: </span>";
+    }
+
+    if ($publication['title']) {
+        echo "<span><strong>" . $publication['title'][0] . "</strong></span>";
+        echo "<span>";
+        if ($publication['patent']) {
+            echo " ; ";
+        } else {
+            echo ". ";
+        }
+        echo "</span>";
+    }
+
+    if ($publication['patent']) {
+        $len_patents = count($publication['patent']);
+        foreach ($publication['patent'] as $index => $patent) {
+            echo "<span>" . $patent['number'] . " ";
+
+            if ($patent['state']) {
+                echo "(" . $patent['state'] . ")";
+            }
+            echo "</span>";
+
+            # last ?
+            if ($index == $len_patents - 1) {
+                echo "<span>. </span>";
+            } else {
+                echo "<span>; </span>";
+            }
+        }
+    }
+
+    if ($publication['publication_year']) {
+        echo "<span>" . $publication['publication_year'][0] . "</span>";
+        echo "<span>. </span>";
+    }
+?>
