c7b961ebf4042aae7a87bd59504b86e802643476
Julien Delasoie <julien.delasoie@epfl.ch>
2018-06-28 14:26:59 +0200

    Update index missing for links-bar

--- data/wp/wp-content/plugins/epfl-infoscience-search/templates/common/links-bar.php
+++ data/wp/wp-content/plugins/epfl-infoscience-search/templates/common/links-bar.php
@@ -1,5 +1,8 @@
 <?php
-if ($thumbnail && $publication['url']['icon'][0]) {
+if ($thumbnail && 
+    isset($publication['url']) &&
+    isset($publication['url']['icon']) &&
+    $publication['url']['icon'][0]) {
     echo '<img class="record-illustration" src="' . $publication['url']['icon'][0] . '" class="record-illustration" alt="n/a">';
 }
 ?>
@@ -9,12 +12,14 @@ if ($thumbnail && $publication['url']['icon'][0]) {
     <?php
         echo '<a class="infoscience_link_detailed" href="https://infoscience.epfl.ch/record/'. $publication['record_id'][0] . '"?ln=en" target="_blank">' . esc_html__("Detailed record", "epfl_infoscience") . '</a>';
 
-        if ($publication['url']['fulltext'][0]) {
+        if (isset($publication['url']['fulltext']) &&
+            $publication['url']['fulltext'][0]) {
             echo ' - ';
             echo '<a class="infoscience_link_fulltext" href="'. $publication['url']['fulltext'][0] . '" target="_blank">'. esc_html__("Full text", "epfl_infoscience") .'</a>';
         }
 
-        if ($publication['doi']) {
+        if (isset($publication['doi']) &&
+            $publication['doi']) {
             echo ' - ';
             echo '<a class="infoscience_link_official" href="https://dx.doi.org/'. $publication['doi'][0] . '" target="_blank">' . esc_html__("View at publisher", "epfl_infoscience") . '</a>';
         }
