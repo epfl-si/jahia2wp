0c42e48fa8453802fba2733ddf78d5ef2787fdb8
Julien Delasoie <julien.delasoie@epfl.ch>
2018-07-09 16:05:38 +0200

    Fix escaping escapes

--- data/wp/wp-content/plugins/epfl-infoscience-search/mathjax-config.php
+++ data/wp/wp-content/plugins/epfl-infoscience-search/mathjax-config.php
@@ -5,7 +5,7 @@ function get_mathjax_config() {
     $to_return .= '    MathJax.Hub.Config(';
     $to_return .= '        {';
     $to_return .= '            tex2jax: {';
-    $to_return .= "                inlineMath: [['$','$'], ['\\(','\\)']],";
+    $to_return .= "                inlineMath: [['$','$'], ['\\\\(','\\\\)']],";
     $to_return .= '                processClass: "tex2jax_process",';
     $to_return .= '                ignoreClass: "no-tex2jax_process",';
     $to_return .= '                }';
