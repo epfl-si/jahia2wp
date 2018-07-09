c4dc745db1c71d698a04c9004b3b93a6ed6a142c
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-07-09 16:18:36 +0000

    Rewrite importer.php as a --require plugin
    Fixes the (unnamed) bug whence bad Polylang is-a-translation-of
    relationship would pop up (in one case, between a page and a media!)
    
    * This naturally lets us load all importers, not just the "official" one -
      This is important for the Polylang hooks found in
      plugins/polylang/modules/plugins/plugins-compat.php to function properly
    
    * just define('WP_LOAD_IMPORTERS', true) and be sure to call the 'admin_init'
      action

--- src/importer.php
+++ src/importer.php
@@ -1,17 +1,43 @@
 <?php
 
-# This script is meant for "wp eval-file".
+#####################################################################
+#
+# Custom XML importer for the jahia2wp project
+#
+# Works a lot like "wp import", but fetches attachments and
+# de-dupes
 #
 # Usage:
 #
-#   wp --path=..... \
-#      eval-file importer.php [ -fetch-attachments ] <wxr_file.xml>
+# wp --path=/path/to/your/WordPress/instance \
+#    --require=/path/to/this/script \
+#    jahia2wp import [ -fetch-attachments ] <file.xml>
+#
+#####################################################################
 
+define('WP_LOAD_IMPORTERS', true);
+define('IMPORT_DEBUG', true);
 
-$importer_plugin_file = WP_PLUGIN_DIR .
-                      '/wordpress-importer/wordpress-importer.php';
-if (! is_file($importer_plugin_file)) {
-    ?>
+WP_CLI::add_command("jahia2wp import", "jahia2wp_import");
+
+function jahia2wp_import ()
+{
+    ob_start("accumulate_and_transform", 1);
+    try {
+        do_import();
+    } finally {
+        ob_flush();
+        ob_end_clean();
+    }
+}
+
+function do_import ()
+{
+    do_action('admin_init');
+    $importer_plugin_file = WP_PLUGIN_DIR .
+                          '/wordpress-importer/wordpress-importer.php';
+    if (! is_file($importer_plugin_file)) {
+        ?>
 
 File <?php echo $importer_plugin_file; ?> Not found
 
@@ -20,43 +46,27 @@ Please install the wordpress-importer plugin, e.g.
   wp --path=... plugin install --activate wordpress-importer
 
 <?php
-    exit(1);
-}
+        exit(1);
+    }
 
-if (function_exists("wordpress_importer_init")) {
-    ?>
-
-Please run with wp-importer plugin skipped (add
---skip-plugins=wordpress-importer to your command line)
-
-<?php
-    exit(1);
-}
-
-global $argv;
-$filename = end($argv); reset($argv);
-if (! is_file($filename)) {
-
-    ?>
+    global $argv;
+    $filename = end($argv); reset($argv);
+    if (! is_file($filename)) {
+        ?>
 File not found: <?php echo $filename; ?>
 
 Usage : wp eval [...] <filename>
-
 <?php
-    exit(1);
+        exit(1);
+    }
+
+    add_filter("wp_import_existing_post", "identify_structural_pages_by_guid", 10, 2);
+    add_action("wp_import_insert_post", "record_guid_of_structural_pages", 10, 4);
+
+    global $wp_import;
+    $wp_import->fetch_attachments = (FALSE !== array_search("-fetch-attachments", $argv));
+    $wp_import->import($filename);
 }
-
-// WP_LOAD_IMPORTERS must be set before loading the plugin (hence why
-// the user must set the command line to skip it; see above)
-define('WP_LOAD_IMPORTERS', true);
-define('IMPORT_DEBUG', true);
-require("$importer_plugin_file");
-wordpress_importer_init();
-
-$fetch_attachments = FALSE !== array_search("-fetch-attachments", $argv);
-add_filter("wp_import_existing_post", "identify_structural_pages_by_guid", 10, 2);
-add_action("wp_import_insert_post", "record_guid_of_structural_pages", 10, 4);
-do_import($filename, $fetch_attachments);
 
 ############################# FUNCTIONS ##################################
 
@@ -79,24 +89,12 @@ function accumulate_and_transform ($buf, $phase) {
     return $out;
 }
 
-function do_import ($filename, $fetch_attachments = false) {
-    global $wp_import;
-    $wp_import->fetch_attachments = $fetch_attachments;
-    ob_start("accumulate_and_transform", 1);
-    try {
-        $wp_import->import($filename);
-    } finally {
-        ob_flush();
-        ob_end_clean();
-    }
-}
-
-# Items with a negative ID have been created by the EPFL XML
-# processing pipeline, not by "wp export". Unlike the default behavior
-# for "normal" pages and posts, identified by their title and creation
-# date, we identify such pages by their <guid> field (in the sense
-# that if we didn't, they could be erroneously imported multiple
-# times)
+# Items with a negative ID - so-called "structural" pages, menus etc.
+# have been created by the EPFL XML processing pipeline, not by "wp
+# export". Unlike the default behavior for "normal" pages and posts,
+# identified by their title and creation date, we de-duplicate structural
+# items by their <guid> field (in the sense that importing the same
+# item again with the same <guid> has no effect).
 
 $EPFL_IMPORT_GUID_META = "epfl-ventilation-guid";
 
