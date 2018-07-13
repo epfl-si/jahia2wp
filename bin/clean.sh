673f5bb87bd36632f6043772821eb26b615dc376
LuluTchab <l.chaboudez@gmail.com>
2018-07-13 09:17:59 +0200

    Change location

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,55 +1,29 @@
 #!/bin/bash
 
+# Import configuration
+CONFIG_FILE="/srv/${WP_ENV}/jahia2wp/src/jahia2wp-utils/config.sh"
+
+if [ ! -e ${CONFIG_FILE} ]
+then
+    echo "Config file (${CONFIG_FILE}) not found!"
+    exit 1
+fi
+source ${CONFIG_FILE}
+
+# Check parameters
 
 
-function cleanVEnv
-{
-
-	htDocsDir=`find volumes/srv/$1/ -name htdocs`
-
-	# If one or more 'htdocs' folder found,
-	if [ "${htDocsDir}" != "" ]
-	then
-
-		# looping through 'htdocs' folder found using FD 9 (http://mywiki.wooledge.org/BashFAQ/089)
-		while read -r path <&9
-		do
-			echo -n "Following folder will be deleted, please confirm (${path}) - (yes/no): "
-			read -r answer
-
-
-			if [ "${answer}" = "yes" ]
-			then
-				echo "Cleaning WP files ($path)..."
-				if [ -e ${path} ]
-				then
-					sudo rm -r ${path}
-				fi
-			else
-				echo "Skipping WP files deletion!"
-			fi
-
-		done 9<<< "${htDocsDir}"
+if [ "$1" == "" ]
+then
+    echo "Site name missing"
+    exit 1
+fi
 
 
 
+python ${SRC_DIR}jahia2wp.py clean ${WP_ENV} ${SITE_ROOT}$1
 
+echo -n "Removing extracted files... "
+rm -rf "${EXPORT_DIR}/$1"
+echo "done"
 
-	else
-		echo "WP files already cleaned!"
-
-	fi
-
-	if [ -e "volumes/db" ]
-	then
-		echo "Cleaning DB files..."
-		sudo rm -r "volumes/db"
-	else
-		echo "DB files already cleaned!"
-	fi
-}
-
-for vEnv in "$@"
-do
-	cleanVEnv ${vEnv}
-done
