6d16d6f56104ddc25d2dcc9dc4a2b7d042696d86
LuluTchab <l.chaboudez@gmail.com>
2018-07-25 10:06:44 +0200

    Remove touiteur

--- src/jahia2wp.py
+++ src/jahia2wp.py
@@ -512,7 +512,6 @@ def export(site, wp_site_url, unit_name, to_wordpress=False, clean_wordpress=Fal
                            'simple-sitemap',
                            'svg-support',
                            'enlighter',
-                           'twitter',
                            'tinymce-advanced',
                            'varnish-http-purge']
 
