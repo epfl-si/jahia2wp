097e48db1625a087c472dfa3dcad3a63e5a0200d
LuluTchab <l.chaboudez@gmail.com>
2018-07-25 14:30:41 +0200

    Change etx char fix func to be more generic and clean also vertical tabs chars

--- src/parser/jahia_site.py
+++ src/parser/jahia_site.py
@@ -28,16 +28,18 @@ This file is named jahia_site to avoid a conflict with Site [https://docs.python
 class Site:
     """A Jahia Site. Have 1 to N Pages"""
 
-    def __init__(self, base_path, name, root_path="", fix_etx_chars=False):
+    def __init__(self, base_path, name, root_path="", fix_problematic_chars=False):
         """
         Create an instance of object
 
         :param base_path: Base path to dir containing extracted Jahia ZIP files
         :param name: Site name
         :param root_path: (optional) ?
-        :param fix_etx_chars: (optional) to tell if we have to fix x03 chars that may be in export_<lang>.xml files.
-                              If this character (=ETX -> End Of Text) is present in a value read by DOM utils, the
-                              string is truncated at x03 position and the following characters are ignored.
+        :param fix_problematic_chars: (optional) to tell if we have to fix problematic characters. Those includes :
+                - x03 chars that may be in export_<lang>.xml files. If this character (=ETX -> End Of Text) is present
+                  in a value read by DOM utils, the string is truncated at x03 position and the following characters
+                  are ignored.
+                - same problem with x0B chars... (vertical tabs)
         """
         # FIXME: base_path should not depend on output-dir
         self.base_path = base_path
@@ -83,9 +85,9 @@ class Site:
                 self.export_files[language] = path
                 self.languages.append(language)
 
-        # If we have to fix ETX char in XML file,
-        if fix_etx_chars:
-            self.fix_etx_chars()
+        # If we have to fix problematic char in XML file,
+        if fix_problematic_chars:
+            self.fix_problematic_chars()
 
         # site params that are parsed later. There are dicts because
         # we have a value for each language. The dict key is the language,
@@ -156,10 +158,10 @@ class Site:
         # generate the report
         self.generate_report()
 
-    def fix_etx_chars(self):
+    def fix_problematic_chars(self):
         """
-        Remove ETX (End of Text) characters from XML files. If this character is present in a value read by DOM utils,
-        the string is truncated at ETX position and the following characters are ignored.
+        Remove problematic chars (End of Text, vertical tabs) characters from XML files. If this character is present
+        in a value read by DOM utils, the string is truncated at ETX position and the following characters are ignored.
         :return:
         """
 
@@ -174,8 +176,8 @@ class Site:
 
             in_file = open(old_export_file, 'rb')
             out_file = open(dom_path, 'wb')
-            # Reading file content, replacing ETX char and writing back to output file
-            out_file.write(in_file.read().replace(b'\x03', b''))
+            # Reading file content, replacing ETX char and Vertical Tabs and writing back to output file
+            out_file.write(in_file.read().replace(b'\x03', b'').replace(b'\x0b', b''))
 
             in_file.close()
             out_file.close()
