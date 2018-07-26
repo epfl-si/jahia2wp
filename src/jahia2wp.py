f306053d6d16b45ea9631cc648c67ce10e6199ae
GregLeBarbar <gregory.charmier.epfl.ch>
2018-07-26 12:53:17 +0200

    delete epfl-science-qa of deactivated_plugins list

--- src/jahia2wp.py
+++ src/jahia2wp.py
@@ -502,7 +502,6 @@ def export(site, wp_site_url, unit_name, to_wordpress=False, clean_wordpress=Fal
                            'epfl-snippet',
                            'epfl-toggle',
                            'epfl-xml',
-                           'epfl-scienceqa',
                            'feedzy-rss-feeds',
                            'remote-content-shortcode',
                            'shortcode-ui',
