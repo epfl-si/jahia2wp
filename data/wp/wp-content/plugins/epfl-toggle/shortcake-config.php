c2b01d65f82e5cd91f189814c776c7bddc07d156
GregLeBarbar <gregory.charmier.epfl.ch>
2018-08-14 17:42:04 +0200

    fix toggle problem

--- data/wp/wp-content/plugins/epfl-toggle/shortcake-config.php
+++ data/wp/wp-content/plugins/epfl-toggle/shortcake-config.php
@@ -19,7 +19,7 @@ Class ToggleShortCakeConfig
             'epfl_toggle',
 
             array(
-                'label' => 'Toggle shortcode',
+                'label' => 'Toggle',
                 'listItemImage' => '<img src="' . plugins_url( 'img/toggle.svg', __FILE__ ) . '" >',
 
                 'attrs'         => array(
