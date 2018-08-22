c581448d5254d5a06d002db25fe9cc1707ddd5a2
GregLeBarbar <gregory.charmier.epfl.ch>
2018-08-22 14:58:49 +0200

    clean epfl-news-2018

--- src/jahia2wp.py
+++ src/jahia2wp.py
@@ -496,7 +496,6 @@ def export(site, wp_site_url, unit_name, to_wordpress=False, clean_wordpress=Fal
                            'epfl-map',
                            'epfl-memento',
                            #'epfl-news',
-                           #'epfl-news-2018',
                            'epfl',
                            'epfl-people',
                            'epfl-scheduler',
