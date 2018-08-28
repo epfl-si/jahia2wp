eb5a1b8536970e255686ddb8181281598c4e66ad
LuluTchab <l.chaboudez@gmail.com>
2018-08-28 10:13:59 +0200

    Typo..:

--- data/wp/wp-content/plugins/epfl-video/epfl-video.php
+++ data/wp/wp-content/plugins/epfl-video/epfl-video.php
@@ -45,7 +45,7 @@ function epfl_video_process_shortcode( $atts, $content = null ) {
     $url = "https://tube.switch.ch/embed/".$video_id;
   }
 
-  return '<div class="container>'.
+  return '<div class="container">'.
          '<div class="epfl-video epfl-video-responsive embed-responsive embed-responsive-16by9">'.
          '<iframe src="'.$url.'" width="'.$width.'" height="'.$height.'" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; encrypted-media" frameborder="0" class="embed-responsive-item"></iframe>'.
          '</div>'.
