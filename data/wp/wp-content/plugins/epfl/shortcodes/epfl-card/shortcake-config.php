0f8738ac1c4fced0bc9df1deed05045c48cbf679
GregLeBarbar <GregLeBarbar@users.noreply.github.com>
2018-09-04 16:30:42 +0200

    epfl-card (#595)
    * epfl-card
    
    * translate + small stuff
    
    * convert text field in textarea + svg icon
    
    * fix textarea

---
+++ data/wp/wp-content/plugins/epfl/shortcodes/epfl-card/shortcake-config.php
@@ -0,0 +1,43 @@
+<?php
+
+Class ShortCakeCardConfig
+{
+    public static function config()
+    {
+        shortcode_ui_register_for_shortcode(
+
+            'epfl_card',
+
+            array(
+                'label'         => __('Card', 'epfl'),
+                'listItemImage' => '<img src="' . plugins_url( 'img/card.svg', __FILE__ ) . '" >',
+                'attrs'         => array(
+                        array(
+                            'label' => '<h3>' . esc_html__('Card title', 'epfl') . '</h3>',
+                            'attr'  => 'title',
+                            'type'  => 'text',
+                        ),
+                        array(
+                            'label' => '<h3>' . esc_html__('Card text', 'epfl') . '</h3>',
+                            'attr'  => 'text',
+                            'type'  => 'textarea',
+                        ),
+                        array(
+                            'label' => '<h3>' . esc_html__('Card link', 'epfl') . '</h3>',
+                            'attr'  => 'link',
+                            'type'  => 'url',
+                        ),
+                        array(
+                            'label'       => '<h3>' . esc_html__('Card image', 'epfl') . '</h3>',
+                            'attr'        => 'image',
+                            'type'        => 'attachment',
+                            'libraryType' => array( 'image' ),
+                        ),
+                    ),
+                'post_type'     => array( 'post', 'page' ),
+            )
+        );
+    }
+}
+
+?>
