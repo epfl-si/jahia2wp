d5232b8aa9a028fa3f3f0937f1424f3a9a8764cc
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-21 13:57:58 +0200

    epfl-faq + fix memento bug

--- data/wp/wp-content/plugins/epfl/epfl.php
+++ data/wp/wp-content/plugins/epfl/epfl.php
@@ -18,6 +18,7 @@ require_once 'shortcodes/epfl-map/epfl-map.php';
 require_once 'shortcodes/epfl-infoscience-search/epfl-infoscience-search.php';
 require_once 'shortcodes/epfl-scheduler/epfl-scheduler.php';
 require_once 'shortcodes/epfl-xml/epfl-xml.php';
+require_once 'shortcodes/epfl-faq/epfl-faq.php';
 
 // load .mo file for translation
 function epfl_load_plugin_textdomain() {
