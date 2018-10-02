6bd60d2492dd321af1cd2a6b826f3df1c9b3d1f3
jdelasoie <julien.delasoie@epfl.ch>
2018-10-02 13:54:06 +0200

    Change the Google Account ID to the www.epfl.ch one

--- data/wp/wp-content/mu-plugins/EPFL_google_analytics_hook.php
+++ data/wp/wp-content/mu-plugins/EPFL_google_analytics_hook.php
@@ -21,7 +21,7 @@ function google_analytics_connector_render() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag("js", new Date());
-    gtag("config", "UA-20398423-1", { "anonymize_ip": true });
+    gtag("config", "UA-4833294-1", { "anonymize_ip": true });
 </script>
 
 <?php
