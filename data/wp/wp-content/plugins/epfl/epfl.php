375728f8ab613cdf8a6d71b8d99e7b7a56cd5d86
jdelasoie <julien.delasoie@epfl.ch>
2018-10-03 14:43:41 +0200

    Epfl share 2018 in epfl (#673)

--- data/wp/wp-content/plugins/epfl/epfl.php
+++ data/wp/wp-content/plugins/epfl/epfl.php
@@ -20,6 +20,7 @@ require_once 'shortcodes/epfl-infoscience-search/epfl-infoscience-search.php';
 require_once 'shortcodes/epfl-scheduler/epfl-scheduler.php';
 require_once 'shortcodes/epfl-xml/epfl-xml.php';
 require_once 'shortcodes/epfl-faq/epfl-faq.php';
+require_once 'shortcodes/epfl-share/epfl-share.php';
 
 // load .mo file for translation
 function epfl_load_plugin_textdomain() {
