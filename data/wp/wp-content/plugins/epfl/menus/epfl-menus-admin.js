51aa20bba586dc821c76bcdf495eb44b10955d07
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-10-23 09:09:06 +0200

    Add documentation

--- data/wp/wp-content/plugins/epfl/menus/epfl-menus-admin.js
+++ data/wp/wp-content/plugins/epfl/menus/epfl-menus-admin.js
@@ -1,3 +1,14 @@
+/**
+ * Interactive features for the external menus functionality
+ *
+ * This piece of JS is loaded on both the list view app for the
+ * epfl-external-menu custom post type, and in the Appearance -> Menus
+ * screen (although at the moment it does nothing on the latter).
+ */
+
+/**
+ * Activate the app on the epfl-external-menu list view screen
+ */
 function initExternalMenuList ($) {
     $('a.page-title-action').remove();
     $('h1.wp-heading-inline').after('<button class="page-title-action">' + wp.translations.refresh_button + '</button>');
