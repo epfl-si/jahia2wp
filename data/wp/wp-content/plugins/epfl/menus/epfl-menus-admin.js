92e49cc8f143767b8052140d1e124cd68743e46d
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-10-23 09:04:15 +0200

    Remove dead code in the JS app

--- data/wp/wp-content/plugins/epfl/menus/epfl-menus-admin.js
+++ data/wp/wp-content/plugins/epfl/menus/epfl-menus-admin.js
@@ -1,27 +1,3 @@
-// It seems that not much documentation exists for the JQuery
-// mini-Web-app that is the Wordpress admin menu editor. However,
-// insofar as Polylang's business in that mini-Web-app is similar
-// to ours, studying the js/nav-menu.js therefrom is helpful.
-
-function initNavMenus ($) {
-  var $metabox = $('div.add-external-menu');
-  $('input.submit-add-to-menu', $metabox).click(function() {
-    console.log("click");
-
-   // For inspiration regarding wpNavMenu, look at wp-admin/js/nav-menu.js
-   wpNavMenu.addItemToMenu(
-     {'-1': {
-       'menu-item-type': 'external-menu',
-       'menu-item-url': 'https://example.com/restapi/menu?lang=en_JP'
-     }},
-     wpNavMenu.addMenuItemToBottom,
-     function() {
-       console.log('Added external menu to menu');
-     });
-
-  });  // submit button's .click()
-}
-
 function initExternalMenuList ($) {
     $('a.page-title-action').remove();
     $('h1.wp-heading-inline').after('<button class="page-title-action">' + wp.translations.refresh_button + '</button>');
@@ -38,10 +14,6 @@ function initExternalMenuList ($) {
 }
 
 jQuery( document ).ready(function($) {
-  if (window.wp.screen.base === 'nav-menus') {
-    initNavMenus($);
-  }
-
   if (window.wp.screen.base === 'edit' && window.wp.screen.post_type === 'epfl-external-menu' ) {
     initExternalMenuList($);
   }
