36d2bd2b666e4bb9adfd0d9fde35b0fd100f04b0
LuluTchab <l.chaboudez@gmail.com>
2018-10-30 08:09:54 +0100

    Simplify .htaccess update

--- data/wp/wp-content/plugins/epfl-intranet/epfl-intranet.php
+++ data/wp/wp-content/plugins/epfl-intranet/epfl-intranet.php
@@ -132,130 +132,16 @@ class Settings extends \EPFL\SettingsBase
   /*************************************************************************************************/
 
    /*
-      BUT : Add/remove .htaccess content, at the specified location
-
-            This function is an enhancement of WordPress function 'insert_with_markers'.
-            https://developer.wordpress.org/reference/functions/insert_with_markers/
-
-      IN  : $insertion     -> Array with lines to insert
-      IN  : $at_beginning  -> To tell if we have to add it at the beginning of the file or not.
+      BUT : Add/remove .htaccess content
    */
    function update_htaccess($insertion, $at_beginning=false)
    {
+
       $filename = get_home_path().'.htaccess';
       $marker = 'EPFL-Intranet';
 
-      if(!file_exists($filename))
-      {
-         if(!is_writable(dirname($filename)))
-         {
-            return false;
-         }
-         if(!touch($filename))
-         {
-            return false;
-         }
-      }
-      elseif(!is_writeable($filename))
-      {
-         return false;
-      }
+      return insert_with_markers($filename, $marker, $insertion);
 
-      if(!is_array($insertion))
-      {
-         $insertion = explode("\n", $insertion);
-      }
-
-      $start_marker = "# BEGIN {$marker}";
-      $end_marker   = "# END {$marker}";
-
-      $fp = fopen($filename, 'r+');
-      if(!$fp)
-      {
-           return false;
-      }
-
-      // Attempt to get a lock. If the filesystem supports locking, this will block until the lock is acquired.
-      flock($fp, LOCK_EX);
-
-      $lines = array();
-      while (!feof($fp))
-      {
-         $lines[] = rtrim(fgets($fp), "\r\n");
-      }
-
-      // Split out the existing file into the preceding lines, and those that appear after the marker
-      $pre_lines = $post_lines = $existing_lines = array();
-      $found_marker = $found_end_marker = false;
-      foreach ($lines as $line)
-      {
-         if(!$found_marker && false !== strpos($line, $start_marker))
-         {
-            $found_marker = true;
-            continue;
-         }
-         elseif(!$found_end_marker && false !== strpos($line, $end_marker))
-         {
-            $found_end_marker = true;
-            continue;
-         }
-
-         if(!$found_marker)
-         {
-            $pre_lines[] = $line;
-         }
-         elseif($found_marker && $found_end_marker)
-         {
-            $post_lines[] = $line;
-         }
-         else
-         {
-            $existing_lines[] = $line;
-         }
-      }
-
-      // Check to see if there was a change
-      if($existing_lines === $insertion)
-      {
-         flock($fp, LOCK_UN);
-         fclose($fp);
-
-         return true;
-      }
-
-      /* Si on doit positionner le contenu au dÃ©but du fichier*/
-      if($at_beginning && !$found_marker)
-      {
-          // Generate the new file data
-         $new_file_data = implode("\n", array_merge(array($start_marker),
-                                                   $insertion,
-                                                   array($end_marker),
-                                                   $pre_lines,
-                                                   $post_lines));
-      }
-      else
-      {
-          // Generate the new file data
-         $new_file_data = implode("\n", array_merge($pre_lines,
-                                                   array($start_marker),
-                                                   $insertion,
-                                                   array($end_marker),
-                                                   $post_lines));
-      }
-
-
-      // Write to the start of the file, and truncate it to that length
-      fseek($fp, 0);
-      $bytes = fwrite($fp, $new_file_data);
-      if($bytes)
-      {
-         ftruncate($fp, ftell($fp));
-      }
-      fflush($fp);
-      flock($fp, LOCK_UN);
-      fclose($fp);
-
-      return (bool) $bytes;
    }
 
 
