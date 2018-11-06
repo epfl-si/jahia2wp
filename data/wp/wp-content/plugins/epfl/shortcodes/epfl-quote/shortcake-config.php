18a6989de5d0fa21c3ea6ad19b1755b60a39d948
GregLeBarbar <GregLeBarbar@users.noreply.github.com>
2018-11-06 09:55:55 +0100

    epfl quote (#625)
    * shortcode epfl-quote
    
    * Translate
    
    * require quote
    
    * Add description
    
    * Add missing context => MErci JUJU
    
    Co-Authored-By: GregLeBarbar <GregLeBarbar@users.noreply.github.com>

---
+++ data/wp/wp-content/plugins/epfl/shortcodes/epfl-quote/shortcake-config.php
@@ -0,0 +1,44 @@
+<?php
+
+Class ShortCakeQuoteConfig
+{
+    public static function config()
+    {
+        shortcode_ui_register_for_shortcode(
+
+            'epfl_quote',
+
+            array(
+                'label'         => __('Quote', 'epfl'),
+                'listItemImage' => '<img src="' . plugins_url( 'img/quote.svg', __FILE__ ) . '" >',
+                'attrs'         => array(
+                        array(
+                            'label' => '<h3>' . esc_html__('Quote', 'epfl') . '</h3>',
+                            'attr'  => 'quote',
+                            'type'  => 'textarea',
+                        ),
+                        array(
+                            'label' => '<h3>' . esc_html__('Source or reference', 'epfl') . '</h3>',
+                            'attr'  => 'cite',
+                            'type'  => 'text',
+                        ),
+                        array(
+                            'label' => '<h3>' . esc_html__('Footer', 'epfl') . '</h3>',
+                            'attr'  => 'footer',
+                            'type'  => 'text',
+                        ),
+                        array(
+                            'label'       => '<h3>' . esc_html__('Quote image', 'epfl') . '</h3>',
+                            'attr'        => 'image',
+                            'type'        => 'attachment',
+                            'libraryType' => array( 'image' ),
+                            'description' => esc_html__('Please, select a square image', 'epfl'),
+                        ),
+                    ),
+                'post_type'     => array( 'post', 'page' ),
+            )
+        );
+    }
+}
+
+?>
