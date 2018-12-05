9e5dc918e2dfc0a74f0492806a0a9681bf1949ad
LuluTchab <l.chaboudez@gmail.com>
2018-12-05 19:35:11 +0100

    Bugfixe... incorrect text

--- data/wp/wp-content/plugins/epfl/lib/prometheus.php
+++ data/wp/wp-content/plugins/epfl/lib/prometheus.php
@@ -19,33 +19,3 @@
     require_once __DIR__."/Prometheus/PushGateway.php";
     require_once __DIR__."/Prometheus/RenderTextFormat.php";
     require_once __DIR__."/Prometheus/Sample.php";
-
-
-/*
-<?php
-
-require_once __DIR__."/../lib/load-prometheus.php";
-
-use Prometheus\CollectorRegistry;
-
-
-$adapter = new Prometheus\Storage\APC();
-
-$registry = new CollectorRegistry($adapter);
-
-
-/**
- * @param string $namespace e.g. cms
- * @param string $name e.g. requests
- * @param string $help e.g. The number of requests made.
- * @param array $labels e.g. ['controller', 'action']
- * @return Counter
- * @throws MetricsRegistrationException
- */
-$counter = $registry->registerCounter('test', 'some_counter', 'it increases', ['type']);
-$counter->incBy(1, ['blue']);
-$counter->incBy(2, ['green']);
-echo "OK\n";
-
-
-*/
