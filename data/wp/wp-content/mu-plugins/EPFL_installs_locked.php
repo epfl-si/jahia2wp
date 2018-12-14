b7910a0895263e45bb89efc9acda9c1751589b75
LuluTchab <l.chaboudez@gmail.com>
2018-12-14 08:12:47 +0100

    update version

--- data/wp/wp-content/mu-plugins/EPFL_installs_locked.php
+++ data/wp/wp-content/mu-plugins/EPFL_installs_locked.php
@@ -3,7 +3,7 @@
  * Plugin Name: EPFL lock plugin and theme install and configuration
  * Plugin URI: 
  * Description: Must-use plugin for the EPFL website.
- * Version: 0.0.2
+ * Version: 0.0.3
  * Author: wwp-admin@epfl.ch
  * */
 
