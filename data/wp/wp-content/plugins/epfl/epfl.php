0a305b5b1e08d12e4ac2413f99722a9ab70cbc40
LuluTchab <l.chaboudez@gmail.com>
2018-12-21 15:11:12 +0100

    Split target URL into 3 differents vars (C2C request)

--- data/wp/wp-content/plugins/epfl/epfl.php
+++ data/wp/wp-content/plugins/epfl/epfl.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: EPFL
  * Description: Provides many epfl shortcodes
- * @version: 1.9
+ * @version: 1.10
  * @copyright: Copyright (c) 2017 Ecole Polytechnique Federale de Lausanne, Switzerland
  */
 
