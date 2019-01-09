93acc532e4a343bf08ec78095fcb13cf4bce45b1
Julien Delasoie <julien.delasoie@epfl.ch>
2019-01-09 09:37:05 +0100

    Remove ref to a removed file #833

--- data/wp/wp-content/plugins/epfl/epfl.php
+++ data/wp/wp-content/plugins/epfl/epfl.php
@@ -30,7 +30,6 @@ require_once 'shortcodes/epfl-tableau/epfl-tableau.php';
 require_once 'shortcodes/epfl-google-forms/epfl-google-forms.php';
 require_once 'shortcodes/epfl-servicenow-search/epfl-servicenow-search.php';
 require_once 'menus/epfl-menus.php';
-require_once 'epfl-multisite.php';
 // Disabled due to 'epfl-intranet' plugin use
 //require_once 'preprod.php';
 
