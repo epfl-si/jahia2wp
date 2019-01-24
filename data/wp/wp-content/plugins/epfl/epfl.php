12cde308cd4958a5c0593234463076ee6bf1a2e6
LuluTchab <l.chaboudez@gmail.com>
2019-01-24 13:21:07 +0100

    Update plugin version

--- data/wp/wp-content/plugins/epfl/epfl.php
+++ data/wp/wp-content/plugins/epfl/epfl.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: EPFL
  * Description: Provides many epfl shortcodes
- * @version: 1.12
+ * @version: 1.13
  * @copyright: Copyright (c) 2017 Ecole Polytechnique Federale de Lausanne, Switzerland
  */
 
