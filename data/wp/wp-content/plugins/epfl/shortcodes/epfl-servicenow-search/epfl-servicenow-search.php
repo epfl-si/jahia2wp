c8ce25423f1a01a615cbc9c58a3f7b4aff91b5f0
LuluTchab <l.chaboudez@gmail.com>
2019-01-31 16:00:34 +0100

    Fix incorrect display

--- data/wp/wp-content/plugins/epfl/shortcodes/epfl-servicenow-search/epfl-servicenow-search.php
+++ data/wp/wp-content/plugins/epfl/shortcodes/epfl-servicenow-search/epfl-servicenow-search.php
@@ -4,15 +4,22 @@
  * Simple display of input field to do a search in ServiceNow (in a new tab)
  */
 function epfl_service_now_search_process_shortcode( $atts, $content = null ) {
+
+ob_start();
+
 ?>
-
-<form onsubmit="$('#snow-search').click()">
-   <input type="text" id="snow-keyword" size="30">
-   <span id="snow-search" class="dashicons dashicons-search" style="font-size:32px;cursor:pointer" onclick="window.open('https://support.epfl.ch/help?id=search&spa=1&q='+$('#snow-keyword').val(), '_blank');"></span>
-</form>
-
+<div class="container-full p-lg-5">
+    <div class="container">
+        <form onsubmit="$('#snow-search').click()">
+           <input type="text" id="snow-keyword" size="30">
+           <span id="snow-search" class="dashicons dashicons-search" style="font-size:32px;cursor:pointer" onclick="window.open('https://support.epfl.ch/help?id=search&spa=1&q='+$('#snow-keyword').val(), '_blank');"></span>
+        </form>
+     </div>
+</div>
 
 <?php
+
+return ob_get_clean();
 }
 
 add_action( 'init', function() {
