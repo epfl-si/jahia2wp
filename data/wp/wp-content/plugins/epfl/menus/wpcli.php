8e8f70273c87ca57698d5fae5445cef4801783d3
Julien Delasoie <julien.delasoie@epfl.ch>
2019-01-31 14:58:09 +0100

    Fix wp cli for menu in local docker

--- data/wp/wp-content/plugins/epfl/menus/wpcli.php
+++ data/wp/wp-content/plugins/epfl/menus/wpcli.php
@@ -19,6 +19,8 @@ add_filter('epfl_rest_rewrite_connect_to', function($hostport, $host, $port) {
     // TODO: this is not a way to go through life, son
     if ($hostport === "www.epfl.ch:443") {
         return "httpd-www:8443";
+    } elseif ($hostport === "jahia2wp-httpd:443") {
+        return "jahia2wp-httpd:8443";
     } else {
         return $hostport;
     }
