a940b1d110fce105e425b16703affa85660fccb3
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-03-04 11:24:24 +0100

    Single scheme to access pods for REST calls
    Turns out, the "no way to go through life" quip was unwarranted; the
    scheme in wpcli.php is the best we have.
    
    * Generalize it (no more duplication between CLI and Web)
    
    * Change parameters to the hook function; as we found out in the case
      of "labs", the host and port are not always enough to take a guess
      where the connection should go

--- data/wp/wp-content/plugins/epfl/menus/wpcli.php
+++ data/wp/wp-content/plugins/epfl/menus/wpcli.php
@@ -15,18 +15,6 @@ use function EPFL\I18N\___;
 require_once(__DIR__ . '/epfl-menus.php');
 use \EPFL\Menus\ExternalMenuItem;
 
-add_filter('epfl_rest_rewrite_connect_to', function($hostport, $host, $port) {
-    // TODO: this is not a way to go through life, son
-    if ($hostport === "www.epfl.ch:443") {
-        return "httpd-www:8443";
-    } elseif ($hostport === "jahia2wp-httpd:443") {
-        return "jahia2wp-httpd:8443";
-    } else {
-        return $hostport;
-    }
-}, 10, 3);
-
-
 class EPFLMenusCLICommand extends WP_CLI_Command
 {
     public static function hook () {
