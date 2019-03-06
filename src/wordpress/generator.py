49caa3a1641595d833d8a7f01cd68d481617ea5a
LuluTchab <l.chaboudez@gmail.com>
2019-03-06 10:53:59 +0100

    Handle mu-plugins folder if exists

--- src/wordpress/generator.py
+++ src/wordpress/generator.py
@@ -452,6 +452,7 @@ class WPGenerator:
         WPMuPluginConfig(self.wp_site, "EPFL_custom_editor_menu.php").install()
         WPMuPluginConfig(self.wp_site, "EPFL_jahia_redirect.php").install()
         WPMuPluginConfig(self.wp_site, "EPFL_google_analytics_hook.php").install()
+        WPMuPluginConfig(self.wp_site, "EPFL_quota_loader.php", plugin_folder="epfl-quota").install()
 
         if self.wp_config.installs_locked:
             WPMuPluginConfig(self.wp_site, "EPFL_installs_locked.php").install()
