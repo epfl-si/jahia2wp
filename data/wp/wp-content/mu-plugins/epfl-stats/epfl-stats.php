62d335d8c0db90f60b34a606e218282c54a8450c
LuluTchab <l.chaboudez@gmail.com>
2019-03-21 08:18:03 +0100

    Plugin version update

--- data/wp/wp-content/mu-plugins/epfl-stats/epfl-stats.php
+++ data/wp/wp-content/mu-plugins/epfl-stats/epfl-stats.php
@@ -1,10 +1,4 @@
 <?PHP
-/**
- * Plugin Name: EPFL-Stats
- * Description: Provide a filter to allow others plugins to log duration of their external call to webservices
- * @version: 1.2
- * @copyright: Copyright (c) 2019 Ecole Polytechnique Federale de Lausanne, Switzerland
- */
 
 require_once 'lib/prometheus.php';
 
