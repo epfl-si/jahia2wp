5d0dd1b715f797e2cebdd7551ffc4e15ea59465f
Dominique Quatravaux <dominique@quatravaux.org>
2019-03-27 14:07:32 +0100

    Refactor into a controller class, with namespace

--- data/wp/wp-content/mu-plugins/epfl-stats/epfl-stats.php
+++ data/wp/wp-content/mu-plugins/epfl-stats/epfl-stats.php
@@ -1,98 +1,110 @@
 <?PHP
 
+namespace EPFL\Stats;
+
 require_once 'lib/prometheus.php';
 
 use Prometheus\CollectorRegistry;
 
-/*
-    Save a webservice call duration including source page and timestamp on which call occurs
-
-    @param $url         -> Webservice URL call
-    @param $duration    -> webservice call duration (seconds with microseconds)
-*/
-function epfl_stats_webservice_call_duration($url, $duration)
+class StatsController
 {
-    /* If we are in CLI mode, it's useless to update in APC because it's the APC for mgmt container and not httpd
-    container */
-    if(php_sapi_name()=='cli') return;
+    public static function hook ()
+    {
+        /* If we are in CLI mode, nobody cares about our stats: */
+        if (static::is_cli()) return;
+        add_action('epfl_stats_webservice_call_duration',
+                   array(get_called_class(), 'prometheus_webservice_perf_counters'),
+                   10, 2);
+        add_action('epfl_stats_media_size_and_count',
+                   array(get_called_class(), 'prometheus_media_gauges'),
+                   10, 3);
+    }
 
-    global $wp;
+    protected static function is_cli ()
+    {
+        return php_sapi_name() == 'cli';
+    }
 
-    $url_details = parse_url($url);
+    /**
+     * Save a webservice call duration including source page and timestamp on which call occurs
+     *
+     * @param $url         Webservice URL call
+     * @param $duration    webservice call duration (seconds with microseconds)
+     */
+    static function prometheus_webservice_perf_counters ($url, $duration)
+    {
 
-    /* Building target host name with scheme */
-    $target_host  = $url_details['scheme']."://".$url_details['host'];
-    if(array_key_exists('port', $url_details) && $url_details['port'] != "") $target_host .= ":".$url_details['port'];
+        global $wp;
 
-    $query = (array_key_exists('query', $url_details))?$url_details['query']:"";
+        $url_details = parse_url($url);
 
-    $adapter = new Prometheus\Storage\APC();
+        /* Building target host name with scheme */
+        $target_host  = $url_details['scheme']."://".$url_details['host'];
+        if(array_key_exists('port', $url_details) && $url_details['port'] != "") $target_host .= ":".$url_details['port'];
 
-    $registry = new CollectorRegistry($adapter);
+        $query = (array_key_exists('query', $url_details))?$url_details['query']:"";
 
-    /* To count time we spend waiting for web services (will disappear in a near future) */
-    $counter = $registry->registerCounter('wp',
-                                      'epfl_shortcode_duration_milliseconds',
-                                      'How long we spend waiting for Web services overall, in milliseconds',
-                                       ['src', 'target_host', 'target_path', 'target_query']);
+        $adapter = new Prometheus\Storage\APC();
 
-    $counter->incBy(floor($duration*1000), [home_url( $wp->request ),
-                                            $target_host,
-                                            $url_details['path'],
-                                            $query]);
+        $registry = new CollectorRegistry($adapter);
 
-    /* To count number of calls to web services */
-    $counter = $registry->registerCounter('wp',
-                                      'epfl_shortcode_ws_call_total',
-                                      'Number of Web service call',
-                                       ['src', 'target_host', 'target_path', 'target_query']);
+        /* To count time we spend waiting for web services (will disappear in a near future) */
+        $counter = $registry->registerCounter('wp',
+                                              'epfl_shortcode_duration_milliseconds',
+                                              'How long we spend waiting for Web services overall, in milliseconds',
+                                              ['src', 'target_host', 'target_path', 'target_query']);
 
-    $counter->inc([home_url( $wp->request ),
-                  $target_host,
-                  $url_details['path'],
-                  $query]);
+        $counter->incBy(floor($duration*1000), [home_url( $wp->request ),
+                                                $target_host,
+                                                $url_details['path'],
+                                                $query]);
 
+        /* To count number of calls to web services */
+        $counter = $registry->registerCounter('wp',
+                                              'epfl_shortcode_ws_call_total',
+                                              'Number of Web service call',
+                                              ['src', 'target_host', 'target_path', 'target_query']);
+
+        $counter->inc([home_url( $wp->request ),
+                       $target_host,
+                       $url_details['path'],
+                       $query]);
+
+    }
+
+    /**
+     *   Save count of nb medias, size usage and quota size
+     *
+     *    @param $used_bytes  Nb bytes used by medias on disk
+     *    @param $quota_bytes Quota size in bytes
+     *   @param $nb_files     Number of medias
+     */
+    static function prometheus_media_gauges ($used_bytes, $quota_bytes, $nb_files)
+    {
+        global $wp;
+
+        $adapter = new Prometheus\Storage\APC();
+
+        $registry = new CollectorRegistry($adapter);
+
+        /* Size information */
+        $size_gauge = $registry->registerGauge('wp',
+                                               'epfl_media_size_bytes',
+                                               'Used (and max) space for medias',
+                                               ['site', 'type']);
+
+        $size_gauge->set($used_bytes, [home_url( $wp->request ), "used"]);
+        $size_gauge->set($quota_bytes, [home_url( $wp->request ), "quota"]);
+
+
+        /* Media count */
+        $count_gauge = $registry->registerGauge('wp',
+                                                'epfl_media_nb_files',
+                                                'Media count on website',
+                                                ['site', 'type']);
+
+        $count_gauge->set($nb_files, [home_url( $wp->request ), "used"]);
+    }
 }
-// We register a new action so others plugins can use it to log webservice call duration
-add_action('epfl_stats_webservice_call_duration', 'epfl_stats_webservice_call_duration', 10, 2);
 
-
-/*
-    Save count of nb medias, size usage and quota size
-
-    @param $used_bytes  -> Nb bytes used by medias on disk
-    @param $quota_bytes -> Quota size in bytes
-    @param $nb_files    -> Number of medias
-*/
-function epfl_stats_media_size_and_count($used_bytes, $quota_bytes, $nb_files)
-{
-    /* If we are in CLI mode, it's useless to update in APC because it's the APC for mgmt container and not httpd
-    container */
-    if(php_sapi_name()=='cli') return;
-
-    global $wp;
-
-    $adapter = new Prometheus\Storage\APC();
-
-    $registry = new CollectorRegistry($adapter);
-
-    /* Size information */
-    $size_gauge = $registry->registerGauge('wp',
-                                           'epfl_media_size_bytes',
-                                           'Used (and max) space for medias',
-                                           ['site', 'type']);
-
-    $size_gauge->set($used_bytes, [home_url( $wp->request ), "used"]);
-    $size_gauge->set($quota_bytes, [home_url( $wp->request ), "quota"]);
-
-
-    /* Media count */
-    $count_gauge = $registry->registerGauge('wp',
-                                           'epfl_media_nb_files',
-                                           'Media count on website',
-                                           ['site', 'type']);
-
-    $count_gauge->set($nb_files, [home_url( $wp->request ), "used"]);
-}
-// We register a new action so others plugins can use it to log webservice call duration
-add_action('epfl_stats_media_size_and_count', 'epfl_stats_media_size_and_count', 10, 3);
+StatsController::hook();
