27e9518fe705a8c8870304615babf7fc68dc6590
Julien Delasoie <julien.delasoie@epfl.ch>
2019-04-01 14:32:53 +0200

    Revert changes on epfl_settings

--- data/wp/wp-content/plugins/EPFL-settings/EPFL-settings.php
+++ data/wp/wp-content/plugins/EPFL-settings/EPFL-settings.php
@@ -7,6 +7,7 @@
  * Text Domain: EPFL-settings
  */
 
+
 function EPFL_settings_load_plugin_textdomain() {
   // wp-content/plugins/plugin-name/languages/EPFL-settings-fr_FR.mo
   load_plugin_textdomain( 'EPFL-settings', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
@@ -14,32 +15,15 @@ function EPFL_settings_load_plugin_textdomain() {
 add_action( 'plugins_loaded', 'EPFL_settings_load_plugin_textdomain' );
 
 
-function validate_breadcrumb($input) {
-    $breadcrumb_option_format = "/(^\[[^\|\[\]]+\|[^\|\[\]]+\]){1}(>(\[[^\|\[\]]+\|[^\|\[\]]+\]){1})*$/";
-    $matched = preg_match($breadcrumb_option_format, $input);
-
-    if ($matched !== 1 && $input !== '') {
-        $error_message = __ ('Incorrect breadcrumb', 'EPFL-settings');
-        add_settings_error(
-            'epfl:custom_breadcrumb',
-            'validationError',
-            $error_message,
-            'error');
-    }
-    // Delete cache entry to force reload of breqdcrumb when option is updated
-    delete_transient('base_breadcrumb');
-    return $input;
-}
-
 function EPFL_settings_register_settings() {
    add_option( 'EPFL_settings_option_name', 'This is my option value.');
    register_setting( 'EPFL_settings_options_group', 'EPFL_settings_option_name', 'EPFL_settings_callback' );
    register_setting( 'EPFL_settings_options_group', 'blogname' );
    register_setting( 'EPFL_settings_options_group', 'blogdescription' );
    register_setting( 'EPFL_settings_options_group', 'WPLANG' );
-   register_setting( 'EPFL_settings_options_group', 'epfl:custom_breadcrumb', 'validate_breadcrumb');
 }
 add_action( 'admin_init', 'EPFL_settings_register_settings' );
+
 
 //add menu EPFL settings under settings menu
 function EPFL_settings_register_options_page() {
@@ -79,12 +63,6 @@ function EPFL_settings_options_page()
       <th scope="row"><label for="plugin:epfl_accred:unit"><?php echo __ ("Accred Unit", 'EPFL-settings');?></label></th>
       <td><label for="plugin:epfl_accred:unit"><?php echo get_option('plugin:epfl_accred:unit'); ?></label></th>
       <p class="description" id="tagline-description"><?php echo __ ("Accred unit allowed to manage this Wordpress site", 'EPFL-settings');?></p>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row"><label for="epfl:custom_breadcrumb"><?php echo __ ("Custom Breadcrumb", 'EPFL-settings');?></label></th>
-      <td><input type="text" id="epfl:custom_breadcrumb" name="epfl:custom_breadcrumb" value="<?php echo get_option('epfl:custom_breadcrumb'); ?>" />
-      <p class="description" id="tagline-description"><?php echo __ ("Format [label|url]>[label|url]>[label|url] (Example : [EPFL|www.epfl.ch]>[ENAC|www.enac.ch])", 'EPFL-settings');?></p>
       </td>
     </tr>
   </table>
