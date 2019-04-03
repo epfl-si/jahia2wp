c1559873103e4cc800b5c31ccbc8d86fb2cb85de
jdelasoie <julien.delasoie@epfl.ch>
2019-04-03 11:32:06 +0200

    Fetch Tags for the current site (#912)
    * Fetch tags from remote
    * Set transient whatever we have wp-vertias online; factorize cache time
    * Improve transient management
    * Order tags

--- data/wp/wp-content/plugins/epfl/epfl.php
+++ data/wp/wp-content/plugins/epfl/epfl.php
@@ -43,4 +43,81 @@ function epfl_load_plugin_textdomain() {
 }
 add_action( 'plugins_loaded', 'epfl_load_plugin_textdomain' );
 
+function epfl_fetch_site_tags_order_cmp($a, $b) {
+  $order = ['faculty', 'institute', 'field-of-research'];
+
+  $a = array_search($a->type, $order);
+  $b = array_search($b->type, $order);
+
+  if ($a == $b) {
+    return 0;
+  }
+
+  return ($a > $b) ? 1 : -1;
+}
+
+/**
+ * Load tags if we are in the labs instance.
+ * Tags are provided from the "Source de vérité" app.
+ *
+ * @return list of tags
+ *
+ */
+function epfl_fetch_site_tags () {
+    $site_url = get_site_url();
+    $tags = NULL;
+    $cache_timeout = 4 * HOUR_IN_SECONDS;
+
+    if ( (defined('WP_DEBUG') && WP_DEBUG) || false === ( $tags = get_transient( 'epfl_custom_tags' ) ) ) {
+      // this code runs when there is no valid transient set
+
+      $tag_provider_url = 'https://wp-veritas.epfl.ch/api/v1';
+      $site = [];
+
+      // first, fetch for the id of this site
+      $site_url = rtrim($site_url, '/');
+      $url_site_to_id = $tag_provider_url . '/sites?site_url=' . rawurlencode($site_url);
+      $site = Utils::get_items($url_site_to_id);
+
+      if ($site === false && !(defined('WP_DEBUG') && WP_DEBUG)) {
+        # wp-veritas is not responding, get the local option and
+        # set a transient, so we dont refresh everytime
+        $tags_and_urls_from_option = get_option('epfl:custom_tags');
+        if ($tags_and_urls_from_option === false) {
+          # no option set ?
+          set_transient( 'epfl_custom_tags', [], $cache_timeout );
+          return;
+        } else {
+            set_transient( 'epfl_custom_tags', $tags_and_urls_from_option, $cache_timeout );
+            return $tags_and_urls_from_option;
+        }
+      } else {
+        # wp-veritas is responding; from the site id, get the tags
+        if (!empty($site)) {
+          $tags_and_urls = []; // [[tag, url], ...]
+          $tags = $site->tags;
+
+          if (!empty($tags)) {
+            # all goods, we have data !
+
+            # order it
+            usort($tags, epfl_fetch_site_tags_order_cmp);
+
+            set_transient( 'epfl_custom_tags', $tags, $cache_timeout);
+            # persist into options too, as a fallback if wp_veritas is no more online
+            update_option('epfl:custom_tags', $tags);
+            return $tags;
+          } else {
+            # nothing for this site ? time to remove local entries
+            set_transient( 'epfl_custom_tags', [], $cache_timeout );
+            delete_option('epfl:custom_tags');
+            return;
+          }
+        }
+      }
+    } else {
+        return $tags;
+    }
+  }
+
 ?>
