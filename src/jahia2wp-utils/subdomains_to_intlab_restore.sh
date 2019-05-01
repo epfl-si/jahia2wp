1198a1b63c9818431bdaa4ef46352cd4e8f65b06
strikerpt <hugo.valente1996@gmail.com>
2019-05-01 13:44:57 +0200

    Correction des scripts de migration et ajout de l'installation et activation du theme 2018

--- src/jahia2wp-utils/subdomains_to_intlab_restore.sh
+++ src/jahia2wp-utils/subdomains_to_intlab_restore.sh
@@ -1,49 +1,65 @@
 #!/bin/sh
 
-echo "Quel est le nom du site ?"
+Quel est le nom du site ?
 read -r site
 
 if [ ${#site} != 0 ]
 
 then
-	#Restauration des fichiers dans le r√pertoire du site
+	#Restauration des fichiers dans le repertoire du site
 	cd /tmp/lab-import/_srv_subdomains_$site.epfl.ch_htdocs/
 
 	tar xzvf /tmp/lab-import/_srv_subdomains_$site.epfl.ch_htdocs/*_full.tar.gz -C /tmp/lab-import/_srv_subdomains_$site.epfl.ch_htdocs
 
-	#D√©placer les fichiers dans le bon r√pertoire
+	#Deplacer les fichiers dans le bon repertoire
 	mv /tmp/lab-import/_srv_subdomains_$site.epfl.ch_htdocs/srv/subdomains/$site.epfl.ch/htdocs /srv/int/migration-wp.epfl.ch/htdocs/labs/$site
-	
-	#Modifier la valeur dans le fichier wp-config.php du site sur INT par db-wwp.epfl.ch par test-db-wwp.epflc.h
+
+	#Modifier la valeur dans le fichier wp-config.php du site sur INT par db-wwp.epfl.ch par test-db-wwp.epfl.ch
 	cd /srv/int/migration-wp.epfl.ch/htdocs/labs/$site
 
 	sed -i 's/db-wwp.epfl.ch/test-db-wwp.epfl.ch/g' /srv/int/migration-wp.epfl.ch/htdocs/labs/$site/wp-config.php
 
-	#Cr√©ation de la base vide
-	
+	#Creation de la base vide
 	DB_USER=`grep DB_USER /srv/int/migration-wp.epfl.ch/htdocs/labs/$site/wp-config.php |awk '{print $3}' |sed "s/'//g"` && DB_PASSWORD=`grep DB_PASSWORD /srv/int/migration-wp.epfl.ch/htdocs/labs/$site/wp-config.php |awk '{print $3}' |sed "s/'//g"` && DB_NAME=`grep DB_NAME /srv/int/migration-wp.epfl.ch/htdocs/labs/$site/wp-config.php |awk '{print $3}' |sed "s/'//g"` && mysql -h test-db-wwp.epfl.ch -u oswproot --password=Pei8vao6Teiv -e "CREATE USER '$DB_USER' IDENTIFIED BY '$DB_PASSWORD';CREATE DATABASE $DB_NAME;GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER';"
 	
-	#Restauration de la base de donn√es
 	wp db import /tmp/lab-import/_srv_subdomains_$site.epfl.ch_htdocs/*.sql --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site
 
 	#Suppression de l'importation des backups
 	rm -rf /tmp/lab-import/_srv_subdomains_$site.epfl.ch_htdocs
 
-	#Changement de .htaccess 
+
+
+
+	#Changement de .htaccess
 	sed -i "s|RewriteBase /|RewriteBase /labs/$site|g" /srv/int/migration-wp.epfl.ch/htdocs/labs/$site/.htaccess
 
-	sed -i "s|RewriteRule . /index.php|RewriteRule . /labs/$site/index.php|g" /srv/int/migration-wp.epfl.ch/htdocs/labs/$site/.htaccess
+sed -i "s|RewriteRule . /index.php|RewriteRule . /labs/$site/index.php|g" /srv/int/migration-wp.epfl.ch/htdocs/labs/$site/.htaccess
 
-
-	#Mettre a jour les URL du site 
+	#Mettre a jour les URL du site
 	wp --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site/ search-replace $site.epfl.ch migration-wp.epfl.ch/labs/$site --skip-columns=guid
 
-	#Mettre le nouveau th√me 2018
-	wp theme install  https://github.com/epfl-idevelop/wp-theme-2018/archive/master.zip --activate --force --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site
+	#Mettre le nouveau theme 2018
+	mkdir /tmp/theme_2018
+	
+	curl -o /tmp/theme_2018/theme_2018.py https://raw.githubusercontent.com/epfl-idevelop/wp-ops/master/docker/wp-base/install-plugins-and-themes.py
+	
+	(cd /tmp/theme_2018/
+	chmod +x theme_2018.py)
 
-	#D√©sintallations des plugins 2010
+	(cd /srv/int/migration-wp.epfl.ch/htdocs/labs/$site/wp-content/themes
+	python /tmp/theme_2018/theme_2018.py theme_2018 https://github.com/epfl-idevelop/wp-theme-2018/tree/dev/wp-theme-2018)
+
+	
+	#Activer le theme 2018
+	wp theme activate wp-theme-2018 --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site
+
+
+	#Suppression de theme_2018.py
+	rm -r /tmp/theme_2018
+	
+	#Desintallations des plugins 2010
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site enlighter
-	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-faq
+	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site EPFL-FAQ
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-google-forms
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-grid
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-infoscience
@@ -53,7 +69,7 @@ then
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-news
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-people
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-scheduler
-	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-snippet
+	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site EPFL-Snippet
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-tableau
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-toggle
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site epfl-twitter
@@ -64,7 +80,6 @@ then
 	wp plugin uninstall --deactivate --path=/srv/int/migration-wp.epfl.ch/htdocs/labs/$site shortcodes-ultimate
 
 	#Installer plugins 2018
-	
 	/srv/int/venv/bin/python /srv/int/jahia2wp/src/jahia2wp.py update-plugins int https://migration-wp.epfl.ch/labs/$site/ --plugin=epfl --extra-config=/srv/int/jahia2wp/functional_tests/extra.yaml --force-plugin
 
 	/srv/int/venv/bin/python /srv/int/jahia2wp/src/jahia2wp.py update-plugins int https://migration-wp.epfl.ch/labs/$site/ --plugin=wp-media-folder --extra-config=/srv/int/jahia2wp/functional_tests/extra.yaml --force-plugin
