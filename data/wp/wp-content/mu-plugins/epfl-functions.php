e74621017221fb27069e9fd90a1135f5d4e17735
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-05-27 14:53:26 +0200

    Reactivate the Polylang cookie on the homepage
    Last week we inadvertently put https://www.epfl.ch back into the
    CloudFlare cache as part of CHG0034820. We had something similar to
    this commit (only cruder) hacked directly into the NFS-side copy of
    epfl-functions.php on that one site; deploying the symlinks undid such
    small changes across the fleet, as intended. We now need to put it
    back.
    
    - On the root site of www.epfl.ch, still allow the Polylang Set-Cookie
      to pass through

--- data/wp/wp-content/mu-plugins/epfl-functions.php
+++ data/wp/wp-content/mu-plugins/epfl-functions.php
@@ -235,8 +235,12 @@ add_shortcode('colored-box', 'colored_box');
 
 --------------------------------------------------------------*/
 
-/* CloudFlare doesn't like the Polylang cookie (or any cookie) */
-define('PLL_COOKIE', false);
+/* CloudFlare doesn't like the Polylang cookie (or any cookie);
+ * however, we still want the homepage to use it (and bypass all
+ * caches). */
+if (site_url() != "https://www.epfl.ch") {
+    define('PLL_COOKIE', false);
+}
 
 /*
     If we have 302 redirection on local address, we transform them to 303 to avoid CloudFlare to cache
