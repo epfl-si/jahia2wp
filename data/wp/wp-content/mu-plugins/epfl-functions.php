dfa03203f2e5341f9a43136a3a74ab21ca3bf8b5
Dominique Quatravaux <dominique@quatravaux.org>
2019-06-07 11:06:59 +0200

    Bump version of epfl-functions.php

--- data/wp/wp-content/mu-plugins/epfl-functions.php
+++ data/wp/wp-content/mu-plugins/epfl-functions.php
@@ -3,7 +3,7 @@
  * Plugin Name: EPFL Functions
  * Plugin URI: 
  * Description: Must-use plugin for the EPFL website.
- * Version: 0.0.8
+ * Version: 0.0.9
  * Author: Aline Keller
  * Author URI: http://www.alinekeller.ch
  */
