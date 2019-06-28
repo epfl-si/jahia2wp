b3e14b3eb7f28c57489cae09d62098868cb2aa19
Julien Delasoie <julien.delasoie@epfl.ch>
2019-06-28 13:54:53 +0200

    Fix infoscience search cache without langage

--- data/wp/wp-content/plugins/epfl/shortcodes/epfl-infoscience-search/epfl-infoscience-search.php
+++ data/wp/wp-content/plugins/epfl/shortcodes/epfl-infoscience-search/epfl-infoscience-search.php
@@ -327,26 +327,29 @@ function epfl_infoscience_search_process_shortcode($provided_attributes = [], $c
         'sort' => $attributes['sort'],
     ];
 
-    # add language to cache definer if a group_by doctype is used
-    if ($group_by === 'doctype' || ($group_by === 'year' && $group_by2 === 'doctype')) {
-        # fetch language
-        # if you can, use the method
-        # use function EPFL\Language\get_current_or_default_language;
+    # fetch language
+    # if you can, use the method
+    # use function EPFL\Language\get_current_or_default_language;
 
-        $default_lang = 'en';
-        $allowed_langs = array('en', 'fr');
-        $language = $default_lang;
-        /* If Polylang installed */
-        if(function_exists('pll_current_language'))
-        {
-            $current_lang = pll_current_language('slug');
-            // Check if current lang is supported. If not, use default lang
-            $language = (in_array($current_lang, $allowed_langs)) ? $current_lang : $default_lang;
-        }
-        $cache_define_by['language'] = $language;
+    $default_lang = 'en';
+    $allowed_langs = array('en', 'fr');
+    $language = $default_lang;
+    /* If Polylang installed */
+    if(function_exists('pll_current_language'))
+    {
+        $current_lang = pll_current_language('slug');
+        // Check if current lang is supported. If not, use default lang
+        $language = (in_array($current_lang, $allowed_langs)) ? $current_lang : $default_lang;
     }
 
+    $cache_define_by['language'] = $language;
+
     $cache_key = md5(serialize($cache_define_by));
+
+    if (is_admin()){
+        # invalidate the cache if we are editing the page
+        delete_transient( $cache_key );
+    }
 
     $page = get_transient($cache_key);
 
