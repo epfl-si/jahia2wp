36040dbc7963cc62ccd555d63d2ac69299d2b8d9
LuluTchab <l.chaboudez@gmail.com>
2019-07-09 14:47:59 +0200

    Ignore symlink while doing inventory

--- src/wordpress/config.py
+++ src/wordpress/config.py
@@ -84,6 +84,12 @@ class WPConfig:
                 logging.debug('checking %s/%s', parent_path, dir_name)
                 try:
                     from_path = os.path.join(parent_path, dir_name)
+
+                    # We may have 'wp' symlink inside WordPress installation, we don't have to follow it.
+                    if os.path.islink(from_path):
+                        logging.debug("path %s is symlink, skipping", from_path)
+                        continue
+
                     wp_site = WPSite.from_path(from_path)
                     if wp_site is None:
                         continue
