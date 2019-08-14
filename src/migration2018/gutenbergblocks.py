d63f3a0c502c01a0449ae214ecb4d2524c464f4f
LuluTchab <l.chaboudez@gmail.com>
2019-08-14 15:14:09 +0200

    Add epfl_infoscience_search, try to be more generic

--- src/migration2018/gutenbergblocks.py
+++ src/migration2018/gutenbergblocks.py
@@ -33,6 +33,8 @@ class GutenbergBlocks(Shortcodes):
     def __get_memento_id(self, memento):
         """
         Returns EPFL Memento ID from name
+
+        :param memento: memento name
         """
 
         # If we don't have information yet
@@ -52,19 +54,59 @@ class GutenbergBlocks(Shortcodes):
         return self.memento_mapping[memento]
 
 
-    def __add_optional_attribute(self, call, attributes, shortcode_attribute, block_attribute=None):
+    def __add_optional_attributes(self, call, attributes, shortcode_attributes):
+        """
+        Updates 'attributes' parameter (dict) with correct value if exists.
 
-        value = self._get_attribute(call, shortcode_attribute)
-        if value:
-            if block_attribute is None:
-                block_attribute = shortcode_attribute
-            attributes[block_attribute] = value
+        :param call: String with shortcode call
+        :param attributes: dict in which we will add attribute value if exists
+        :param shortcode_attributes: List with either attributes names (string) or dict with shortcode attribute
+                name as key an as value, the attribute name we have to use for Gutenberg Block. If string, we
+                assume that Gutenberg attribute is the same as the one in the shortcode.
+        """
+        for attr in shortcode_attributes:
+
+            # If it's a dictionnary, we have to recover shortcode attribute name and block attribute name
+            if isinstance(attr, dict):
+                shortcode_attr = list(attr)[0]
+                block_attr = attr[shortcode_attr]
+            else:
+                shortcode_attr = block_attr = attr
+
+            value = self._get_attribute(call, shortcode_attr)
+            if value:
+                attributes[block_attr] = value
+
+
+    def __add_one_to_one_attributes(self, call, attributes, shortcode_attributes):
+        """
+        Update 'attributes' parameter (dict) for parameters we can simply recover value from shortcode call
+
+        :param call: String with shortcode call
+        :param attributes: Dict in which we will add values
+        :param shortcode_attributes: List with either attributes names (string) or dict with shortcode attribute
+                name as key an as value, the attribute name we have to use for Gutenberg Block. If string, we
+                assume that Gutenberg attribute is the same as the one in the shortcode.
+        :return:
+        """
+
+        for attr in shortcode_attributes:
+            
+            # If it's a dictionnary, we have to recover shortcode attribute name and block attribute name
+            if isinstance(attr, dict):
+                shortcode_attr = list(attr)[0]
+                block_attr = attr[shortcode_attr]
+            else:
+                shortcode_attr = block_attr = attr
+
+            attributes[block_attr] = self._get_attribute(call, shortcode_attr)
 
 
     def _fix_epfl_news_2018(self, content):
         """
         Transforms EPFL news 2018 shortcode to Gutenberg block
 
+        :param content: String with page content to update
         """
         shortcode = 'epfl_news_2018'
         block = 'epfl/news'
@@ -79,26 +121,31 @@ class GutenbergBlocks(Shortcodes):
         # Looking for all calls to modify them one by one
         calls = self._get_all_shortcode_calls(content, shortcode)
 
+        # For attributes we simply need to recover
+        one_to_one_recover = ['channel', 
+                              'lang', 
+                              'category']
+
         for call in calls:
 
             # To store new attributes
             attributes = {}
 
             # Generating new attributes
-            attributes['channel'] = self._get_attribute(call, 'channel')
-
+            
             template = self._get_attribute(call, 'template')
             if template not in templates_mapping:
                 raise "Undefined {} template! {}".format(shortcode, template)
             attributes['template'] = templates_mapping[template]
 
             if template == '1':
-                attributes['nbNews'] = self._get_attribute(call, 'nb_news')
-
-            attributes['lang'] = self._get_attribute(call, 'lang')
-            
+                one_to_one_recover.append({'nb_news': 'nbNews'})
+                
             attributes['displayLinkAllNews'] = self._get_attribute(call, 'all_news_link').lower() == 'true'
-            attributes['category'] = self._get_attribute(call, 'category')
+
+            # Recovering attributes from shortcode
+            self.__add_one_to_one_attributes(call, attributes, one_to_one_recover)
+
 
             # TODO: also handle 'themes' attribute, which is not correctly documented on Confluence... 
             logging.warning("Handle 'themes' attribute !!")
@@ -121,6 +168,7 @@ class GutenbergBlocks(Shortcodes):
         """
         Transforms EPFL news 2018 shortcode to Gutenberg block
 
+        :param content: String with page content to update
         """
         shortcode = 'epfl_memento_2018'
         block = 'epfl/memento'
@@ -132,6 +180,12 @@ class GutenbergBlocks(Shortcodes):
 
         # Looking for all calls to modify them one by one
         calls = self._get_all_shortcode_calls(content, shortcode)
+
+        # For attributes we simply need to recover
+        one_to_one_recover = ['lang', 
+                              'category', 
+                              'period', 
+                              'keyword']
 
         for call in calls:
 
@@ -146,10 +200,8 @@ class GutenbergBlocks(Shortcodes):
                 raise "Undefined {} template! {}".format(shortcode, template)
             attributes['template'] = templates_mapping[template]
 
-            attributes['lang'] = self._get_attribute(call, 'lang')
-            attributes['category'] = self._get_attribute(call, 'category')
-            attributes['period'] = self._get_attribute(call, 'period')
-            attributes['keyword'] = self._get_attribute(call, 'keyword')
+            # Recovering attributes from shortcode
+            self.__add_one_to_one_attributes(call, attributes, one_to_one_recover)
 
             # Add new attributes
             attributes['nbEvents'] = 3
@@ -172,6 +224,7 @@ class GutenbergBlocks(Shortcodes):
         """
         Transforms EPFL people 2018 shortcode to Gutenberg block
 
+        :param content: String with page content to update
         """
         shortcode = 'epfl_people_2018'
         block = 'epfl/people'
@@ -179,17 +232,96 @@ class GutenbergBlocks(Shortcodes):
         # Looking for all calls to modify them one by one
         calls = self._get_all_shortcode_calls(content, shortcode)
 
+        # For attributes we simply need to recover
+        one_to_one_recover = ['columns']
+        # For optional attributes 
+        optional_attributes = ['units', 
+                               'scipers', 
+                               'function', 
+                               {'doctoral_program': 'doctoralProgram'}]
+
         for call in calls:
 
             # To store new attributes
             attributes = {}
 
-            self.__add_optional_attribute(call, attributes, 'units')
-            self.__add_optional_attribute(call, attributes, 'scipers')
-            self.__add_optional_attribute(call, attributes, 'doctoral_program', 'doctoralProgram')
-            self.__add_optional_attribute(call, attributes, 'function')
+            self.__add_optional_attributes(call, attributes, optional_attributes)
+            
+            # Recovering attributes from shortcode
+            self.__add_one_to_one_attributes(call, attributes, one_to_one_recover)
 
-            attributes['columns'] = self._get_attribute(call, 'columns')
+            # We generate new shortcode from scratch
+            new_call = '<!-- wp:{} {} /-->'.format(block, json.dumps(attributes))
+
+            logging.info("Before: %s", call)
+            logging.info("After: %s", new_call)
+
+            # Replacing in global content
+            content = content.replace(call, new_call)
+            
+            self._update_report(shortcode)
+
+        return content
+
+
+    def _fix_epfl_infoscience_search(self, content):
+        """
+        Transforms EPFL Infoscience Search shortcode to Gutenberg block
+        https://github.com/epfl-idevelop/jahia2wp/blob/release2018/data/wp/wp-content/plugins/epfl/shortcodes/epfl-infoscience-search/epfl-infoscience-search.php
+
+        :param content: String with page content to update
+        """
+        shortcode = 'epfl_infoscience_search'
+        block = 'epfl/infoscience-search'
+
+        # Looking for all calls to modify them one by one
+        calls = self._get_all_shortcode_calls(content, shortcode, with_content=True)
+
+        # For attributes we simply need to recover
+        one_to_one_recover = ['pattern',
+                              'limit', 
+                              'sort', 
+                              'collection', 
+                              'pattern2', 
+                              'field2', 
+                              {'field': 'fieldRestriction'}, 
+                              'operator2', 
+                              'pattern3', 
+                              'field3', 
+                              'operator3', 
+                              'format']
+
+        for call in calls:
+
+            # To store new attributes
+            attributes = {}
+
+            attributes['url'] = self._get_content(call)
+
+            # Recovering attributes from shortcode
+            self.__add_one_to_one_attributes(call, attributes, one_to_one_recover)
+
+            attributes['summary'] = self._get_attribute(call, 'summary').lower() == 'true'
+            attributes['thumbnail'] = self._get_attribute(call, 'thumbnail').lower() == 'true'
+            
+            # Handling 'groupBy' speciality
+            group_by = self._get_attribute(call, 'group_by')
+            group_by2 = self._get_attribute(call, 'group_by2')
+
+            if not group_by or group_by == '':
+                group_by_final = None
+            elif group_by == 'year':
+                if not group_by2 or group_by2 == '':
+                    group_by_final = 'year'
+                elif group_by2 == 'doctype':
+                    group_by_final = 'year_doctype'
+            elif group_by == 'doctype':
+                if not group_by2 or group_by2 == '':
+                    group_by_final = 'doctype'
+                elif group_by2 == 'year':
+                    group_by_final = 'doctype_year'
+            
+            attributes['groupBy'] = group_by_final
 
             # We generate new shortcode from scratch
             new_call = '<!-- wp:{} {} /-->'.format(block, json.dumps(attributes))
