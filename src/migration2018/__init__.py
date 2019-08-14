91f59f4754d48b0744a2ac6ed30f5bc5b5e25566
LuluTchab <l.chaboudez@gmail.com>
2019-08-14 10:44:20 +0200

    Some inits

--- src/migration2018/__init__.py
+++ src/migration2018/__init__.py
@@ -1,1 +1,8 @@
 """(c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2018"""
+from .shortcodes import Shortcodes
+from .gutenbergblocks import GutenbergBlocks
+
+__all__ = [
+    Shortcodes,
+    GutenbergBlocks
+]
