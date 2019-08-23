4abe8644c4da1d4be6bc173b6535ee5c73a064d6
Julien Delasoie <julien.delasoie@epfl.ch>
2019-08-23 11:05:20 +0200

    Add gutenberg limiter scripts

--- data/wp/wp-content/mu-plugins/EPFL_custom_editor_menu.php
+++ data/wp/wp-content/mu-plugins/EPFL_custom_editor_menu.php
@@ -1,9 +1,9 @@
 <?php
 /*
-* Plugin Name: EPFL custom editor role menu 
-* Plugin URI: 
+* Plugin Name: EPFL custom editor role menu
+* Plugin URI:
 * Description: Must-use plugin for the EPFL website.
-* Version: 0.0.1
+* Version: 1.0.1
 * Author: wwp-admin@epfl.ch
  */
 
@@ -14,11 +14,25 @@ $role_object->add_cap( 'edit_theme_options' );
 function hide_menu() {
 	// Hide theme selection page
 	remove_submenu_page( 'themes.php', 'themes.php' );
- 
+
 	// Hide customize page
 	global $submenu;
 	unset($submenu['themes.php'][6]);
 }
- 
+
 add_action('admin_head', 'hide_menu');
+
+// For Gutenberg only, limit and remove some elements
+function add_gutenberg_custom_editor_menu() {
+	wp_enqueue_script(
+		'wp-gutenberg-epfl-custom-editor-menu',
+		content_url() . '/mu-plugins/EPFL_gutenberg_custom_editor.js',
+		array( 'wp-editor', 'wp-blocks', 'wp-i18n', 'wp-element' )
+	);
+}
+
+// Gutenberg is on ?
+if (function_exists( 'register_block_type' ) ) {
+	add_action( 'enqueue_block_editor_assets', 'add_gutenberg_custom_editor_menu' );
+}
 ?>
