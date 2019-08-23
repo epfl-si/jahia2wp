4ea60fbb3cc4098e2bc0908373509cc551d5b25a
Julien Delasoie <julien.delasoie@epfl.ch>
2019-08-23 11:09:37 +0200

    Add allowed blocks as mu plugins

--- data/wp/wp-content/mu-plugins/EPFL_custom_editor_menu.php
+++ data/wp/wp-content/mu-plugins/EPFL_custom_editor_menu.php
@@ -31,8 +31,52 @@ function add_gutenberg_custom_editor_menu() {
 	);
 }
 
+function my_plugin_allowed_block_types( $allowed_block_types, $post ) {
+    $blocks = array(
+        'epfl/news',
+        'epfl/memento',
+        'epfl/cover',
+        'epfl/cover-dynamic',
+        'epfl/toggle',
+        'epfl/quote',
+        'epfl/people',
+        'epfl/map',
+        'epfl/introduction',
+        'epfl/hero',
+        'epfl/google-forms',
+        'epfl/video',
+        'epfl/scheduler',
+        'epfl/tableau',
+        'epfl/page-teaser',
+        'epfl/custom-teaser',
+        'epfl/custom-highlight',
+        'epfl/page-highlight',
+        'epfl/post-teaser',
+        'epfl/post-highlight',
+        'epfl/infoscience-search',
+        'epfl/social-feed',
+        'epfl/contact',
+        'epfl/caption-cards',
+        'epfl/card',
+        'epfl/definition-list',
+        'epfl/links-group',
+        'core/paragraph',
+        'core/heading',
+    );
+
+    // Add epfl/scienceqa block for WP instance https://www.epfl.ch only
+    if (get_option('blogname') == 'EPFL') {
+        array_push($blocks, 'epfl/scienceqa');
+    }
+
+  	return $blocks;
+    // return True; // if you want all natifs blocks.
+}
+
 // Gutenberg is on ?
 if (function_exists( 'register_block_type' ) ) {
 	add_action( 'enqueue_block_editor_assets', 'add_gutenberg_custom_editor_menu' );
+	add_filter( 'allowed_block_types', 'my_plugin_allowed_block_types', 10, 2 );
 }
+
 ?>
