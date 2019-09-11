c6e863c7cbafb00cf96e253946e0a62dc3770694
LuluTchab <l.chaboudez@gmail.com>
2019-09-11 10:07:12 +0200

    Add EPFL Custom Highlight

--- src/migration2018/gutenbergblocks.py
+++ src/migration2018/gutenbergblocks.py
@@ -1301,6 +1301,61 @@ class GutenbergBlocks(Shortcodes):
 
         return content
 
+    
+    def _fix_epfl_custom_highlight(self, content, page_id):
+        """
+        Transforms EPFL Hero shortcode to Gutenberg block
+
+        :param content: content to update
+        :param page_id: Id of page containing content
+        """
+        shortcode = 'epfl_custom_highlight'
+        block = 'epfl/custom-highlight'
+
+        # Looking for all calls to modify them one by one
+        calls = self._get_all_shortcode_calls(content, shortcode)
+
+        # Attribute description to recover correct value from each shortcode calls
+        attributes_desc = [ 'title',
+                            'description',
+                            'link',
+                            'layout',
+                            {
+                                'shortcode': 'buttonlabel',
+                                'block': 'buttonLabel'
+                            },
+                            {
+                                'shortcode': 'image',
+                                'block': 'imageId'
+                            },
+                            { 
+                                'shortcode': 'image',
+                                'block': 'imageUrl',
+                                'map_func': '_get_image_url'
+                            }]
+        
+
+        for call in calls:
+
+            # To store new attributes
+            attributes = {}
+
+            # Recovering attributes from shortcode
+            self.__add_attributes(call, attributes, attributes_desc, page_id)
+
+            # We generate new shortcode from scratch
+            new_call = '<!-- wp:{} {} /-->'.format(block, json.dumps(attributes))
+
+            self._log_to_file("Before: {}".format(call))
+            self._log_to_file("After: {}".format(new_call))
+
+            # Replacing in global content
+            content = content.replace(call, new_call)
+            
+            self._update_report(shortcode)
+
+        return content
+
 
     def fix_site(self, openshift_env, wp_site_url, shortcode_name=None, simulation=False):
         """
