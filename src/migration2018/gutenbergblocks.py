8ad3ea37277ce8fdb05033c91695fd43beed5b03
LuluTchab <l.chaboudez@gmail.com>
2019-09-17 13:30:40 +0200

    Add EPFL Video

--- src/migration2018/gutenbergblocks.py
+++ src/migration2018/gutenbergblocks.py
@@ -1813,7 +1813,46 @@ class GutenbergBlocks(Shortcodes):
             
             self._update_report(shortcode)
 
-        return content    
+        return content   
+
+    
+    def _fix_epfl_video(self, content, page_id):
+        """
+        Transforms EPFL Video shortcode to Gutenberg block
+
+        :param content: content to update
+        :param page_id: Id of page containing content
+        """
+        shortcode = 'epfl_video'
+        block = 'epfl/video'
+
+        # Looking for all calls to modify them one by one
+        calls = self._get_all_shortcode_calls(content, shortcode)
+
+        # Attribute description to recover correct value from each shortcode calls
+        attributes_desc = ['url']
+        
+
+        for call in calls:
+
+            # To store new attributes
+            attributes = {}
+
+            # Recovering attributes from shortcode
+            self.__add_attributes(call, attributes, attributes_desc, page_id)
+
+            # We generate new shortcode from scratch
+            new_call = '<!-- wp:{} {} /-->'.format(block, json.dumps(attributes))
+
+            self._log_to_file("Before: {}".format(call))
+            self._log_to_file("After: {}".format(new_call))
+
+            # Replacing in global content
+            content = content.replace(call, new_call)
+            
+            self._update_report(shortcode)
+
+        return content  
 
 
     def fix_site(self, openshift_env, wp_site_url, shortcode_name=None, simulation=False):
