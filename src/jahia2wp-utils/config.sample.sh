73fa3e6f9db38e5e0ee972d18087a8bd6e54bfb6
LuluTchab <l.chaboudez@gmail.com>
2019-09-23 11:03:41 +0200

    Adapt content and help

--- src/jahia2wp-utils/config.sample.sh
+++ src/jahia2wp-utils/config.sample.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 
-set -x
 # Copy file as "config.sh" and edit path if necessary
 
 EXPORT_DIR="/srv/${WP_ENV}/jahia2wp/data/jahia-data"
 SRC_DIR="/srv/${WP_ENV}/jahia2wp/src/"
 CSV_FILE="/srv/${WP_ENV}/jahia2wp/data/csv/migrations.csv"
-SITE_ROOT="https://migration-wp.epfl.ch/"
+SITE_ROOT="https://jahia2wp-httpd/"
+
+#######################################
+# Don't edit after this line
+
+# Recovering from config
 
 ZIP_DIR="`echo ${JAHIA_ZIP_PATH}`"
 if [ "${ZIP_DIR}" == "." ]
