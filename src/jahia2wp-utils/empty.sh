0870b5397ff2ad0ba5ff9374e96630d188fbb6eb
LuluTchab <l.chaboudez@gmail.com>
2019-09-23 11:01:27 +0200

    Generate empty site in WPENV

---
+++ src/jahia2wp-utils/empty.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Import configuration
+CONFIG_FILE="/srv/${WP_ENV}/jahia2wp/src/jahia2wp-utils/config.sh"
+
+if [ ! -e ${CONFIG_FILE} ]
+then
+    echo "Config file (${CONFIG_FILE}) not found!"
+    exit 1
+fi
+source ${CONFIG_FILE}
+
+# Check parameters
+
+
+if [ "$1" == "" ]
+then
+    echo "Site name missing"
+    exit 1
+fi
+
+
+python jahia2wp.py generate lchaboudez https://jahia2wp-httpd/${1} --extra-config=/srv/${WP_ENV}/jahia2wp/functional_tests/extra.yaml --nosymlink $2
