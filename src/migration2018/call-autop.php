d23312fbd1a9cfbd1daa49dfa02911efc0fdee33
Grégoy Charmier <gregory.charmier@gmail.com>
2019-09-27 17:05:37 +0200

    fix

--- src/migration2018/call-autop.php
+++ src/migration2018/call-autop.php
@@ -2,7 +2,7 @@
 
 $filename = $args[0];
 $content = file_get_contents( $filename );
-$content = "toto";
+//$content = "toto";
 $content = wpautop( $content );
 
 // Soit on save dans le fichier directement 
