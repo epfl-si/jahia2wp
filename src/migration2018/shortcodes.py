b2654d8e2982ac27ce070ef5dc85a63dec1acd3e
Grégoy Charmier <gregory.charmier@gmail.com>
2019-09-27 12:28:54 +0200

    debug

--- src/migration2018/shortcodes.py
+++ src/migration2018/shortcodes.py
@@ -1229,7 +1229,9 @@ class Shortcodes():
                             
                                 # call autop
                                 php_autop = "{}/call-autop.php".format(os.path.dirname(os.path.realpath(__file__)))
-                                content_with_p = Utils.run_command("wp eval-file {} {} --path={}".format(php_autop, content.encode(), self.wp_site.path))
+                                cmd = "wp eval-file {} {} --path={}".format(php_autop, content.encode(), self.wp_site.path)
+                                logging.debug(cmd)
+                                content_with_p = Utils.run_command(cmd)
                                 logging.debug(content_with_p)
 
                                 # UPDATE THIS LINE with content_with_p                             
