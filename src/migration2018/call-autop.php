635ec55cdc068c42ca92e78455c6debbe15456a3
Grégoy Charmier <gregory.charmier@gmail.com>
2019-09-27 15:28:01 +0200

    fix

--- src/migration2018/call-autop.php
+++ src/migration2018/call-autop.php
@@ -2,9 +2,10 @@
 
 $filename = $args[0];
 $content = file_get_contents( $filename );
-$content = wpautop( $content );
+$content = wpautop( $content, false );
 
 // Soit on save dans le fichier directement 
+file_put_contents($filename, $content);
 // Soit on retourne via un 
-echo $content;
+//echo $content;
 ?>
