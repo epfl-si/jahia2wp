b8c8a11a871ba4260f42ee50e111e091d00f8f5d
LuluTchab <l.chaboudez@gmail.com>
2019-11-21 13:03:35 +0100

    Transform mu plugin to have a loader (because several files)

---
+++ data/wp/wp-content/mu-plugins/EPFL_custom_editor_menu_loader.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* Plugin Name: EPFL custom editor role menu
+* Plugin URI:
+* Description: Must-use plugin for the EPFL website.
+* Version: 1.0.5
+* Author: wwp-admin@epfl.ch
+ */
+
+
+
+require (WPMU_PLUGIN_DIR.'/epfl-custom-editor-menu/epfl-custom-editor-menu.php');
