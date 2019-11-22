7e84f87d6ddd8baca239879fa11dc425dc553370
LuluTchab <l.chaboudez@gmail.com>
2019-11-22 13:33:48 +0100

    Move lock to right file

--- data/wp/wp-content/mu-plugins/EPFL_custom_editor_menu_loader.php
+++ data/wp/wp-content/mu-plugins/EPFL_custom_editor_menu_loader.php
@@ -3,7 +3,7 @@
 * Plugin Name: EPFL custom editor role menu
 * Plugin URI:
 * Description: Must-use plugin for the EPFL website.
-* Version: 1.0.5
+* Version: 1.0.6
 * Author: wwp-admin@epfl.ch
  */
 
--- data/wp/wp-content/mu-plugins/epfl-custom-editor-menu/epfl-custom-editor-menu.php
+++ data/wp/wp-content/mu-plugins/epfl-custom-editor-menu/epfl-custom-editor-menu.php
@@ -1,19 +1,5 @@
 <?php
 
-// Allow editors to see Appearance menu
-$role_object = get_role( 'editor' );
-$role_object->add_cap( 'edit_theme_options' );
-function hide_menu() {
-	// Hide theme selection page
-	remove_submenu_page( 'themes.php', 'themes.php' );
-
-	// Hide customize page
-	global $submenu;
-	unset($submenu['themes.php'][6]);
-}
-
-add_action('admin_head', 'hide_menu');
-
 // For Gutenberg only, limit and remove some elements
 function add_gutenberg_custom_editor_menu() {
 	wp_enqueue_script(
